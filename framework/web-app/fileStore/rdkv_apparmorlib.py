d635b402a304960b22ac88dda7f3636389878d32
ssimon420 <shinu_simon@comcast.com>
2023-02-01 19:00:05 +0530

    RDKTT-4036 : Move Apparmor scripts under components folder
    Move Apparmor scripts under components folder
    Reason for change: Feature enhancements
    Test Procedure: Check if test success with TDK
    Risks: None
    Signed-off-by:Shinu S<shinu.s@tataelxsi.co.in>
    
    Signed-off-by: ssimon420 <shinu_simon@comcast.com>
    (cherry picked from commit d84d5b31d887984aa80144932624a10ea57b6269)
    Change-Id: I8eaf756b1773f80e1ce665c68d7b4c2efc32a5bd

---
+++ framework/web-app/fileStore/rdkv_apparmorlib.py
@@ -0,0 +1,253 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2023 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#########################################################################
+import requests
+import json
+import time
+import os
+import subprocess
+import inspect
+import ConfigParser
+from SSHUtility import *
+
+deviceIP=""
+devicePort=""
+deviceName=""
+deviceType=""
+
+#METHODS
+#---------------------------------------------------------------
+#INITIALIZE THE MODULE
+#---------------------------------------------------------------
+def init_module (libobj, port, deviceInfo):
+    global deviceIP
+    global devicePort
+    global deviceName
+    global deviceType
+    deviceIP = libobj.ip;
+    devicePort = port
+    deviceName = deviceInfo["devicename"]
+    deviceType = deviceInfo["boxtype"]
+
+#----------------------------------------------------------------------
+#GET DEVICE CONFIGURATION FROM THE DEVICE CONFIG FILE
+# Description  : Read the value of device configuration from the <device>.config file
+# Parameters   : basePath - a string to specify the path of the TM
+#                configKey - a string to specify the configuration whose value needs to be retrieved from the <device>.config file
+# Return Value : value of device configuration or error log in case of failure
+#----------------------------------------------------------------------
+def rdkvapparmor_getDeviceConfig (basePath, configKey):
+    deviceConfigFile=""
+    configValue = ""
+    output = ""
+    configPath = basePath + "/"   + "fileStore/tdkvRDKServiceConfig"
+    deviceNameConfigFile = configPath + "/" + deviceName + ".config"
+    deviceTypeConfigFile = configPath + "/" + deviceType + ".config"
+    # Check whether device / platform config files required for
+    # executing the test are present
+    if os.path.exists (deviceNameConfigFile) == True:
+        deviceConfigFile = deviceNameConfigFile
+    elif os.path.exists (deviceTypeConfigFile) == True:
+        deviceConfigFile = deviceTypeConfigFile
+    else:
+        output = "FAILURE : No Device config file found : " + deviceNameConfigFile + " or " + deviceTypeConfigFile
+        print output
+        #print "[ERROR]: No Device config file found : %s or %s" %(deviceNameConfigFile,deviceTypeConfigFile)
+    try:
+        if (len (deviceConfigFile) != 0) and (len (configKey) != 0):
+            config = ConfigParser.ConfigParser ()
+            config.read (deviceConfigFile)
+            deviceConfig = config.sections ()[0]
+            configValue =  config.get (deviceConfig, configKey)
+            output = configValue
+        else:
+            output = "FAILURE : DeviceConfig file or key cannot be empty"
+            print output
+    except Exception as e:
+        output = "FAILURE : Exception Occurred: [" + inspect.stack()[0][3] + "] " + e.message
+        print output
+    return output;
+
+#-----------------------------------------------------------------
+#TO OBTAIN CREDENTIALS FROM CONFIG FILE
+#-----------------------------------------------------------------
+def obtainCredentials(obj,key_list):
+    print "Retrieving Configuration values from config file......."
+    configKeyList = key_list
+    configValues = {}
+    value_list = []
+    tdkTestObj = obj.createTestStep('rdkvapparmor_getDeviceConfig')
+    #Get each configuration from device config file
+    if True:
+        for configKey in configKeyList:
+            tdkTestObj.addParameter("basePath",obj.realpath)
+            tdkTestObj.addParameter("configKey",configKey)
+            tdkTestObj.executeTestCase("SUCCESS")
+            configValues[configKey] = tdkTestObj.getResultDetails()
+            if "FAILURE" not in configValues[configKey] and configValues[configKey] != "":
+                print "SUCCESS: Successfully retrieved %s configuration from device config file" %(configKey)
+                tdkTestObj.setResultStatus("SUCCESS")
+                result = "SUCCESS"
+            else:
+                print "FAILURE: Failed to retrieve %s configuration from device config file" %(configKey)
+                if configValues[configKey] == "":
+                    print "\n Please configure the %s key in the device config file" %(configKey)
+                    tdkTestObj.setResultStatus("FAILURE")
+                result = "FAILURE"
+                break
+    else:
+        print "Unable to get configuration values"
+        result = "FAILURE"
+    if "FAILURE" not in result:
+        if configValues["SSH_PASSWORD"] == "None":
+           configValues["SSH_PASSWORD"] = ""
+        return configValues
+    else:
+        print "FAILURE: Failed to get configuration values"
+        tdkTestObj.setResultStatus("FAILURE");
+        exit()
+
+#-------------------------------------------------------------------
+#EXECUTE A COMMAND IN DUT SHELL AND GET THE OUTPUT
+# Description  : Execute a command in DUT through ssh_and_execute() from SSHUtility library and get the output
+# Parameters   : 1. sshMethod -  string to specify the SSH method to be used
+#                2. credentials - a coma ceparated string to specify the parameters for ssh_and_execute() method. Values are retrieved from <device>.config
+#                       a. credentials[0] - string to specify the DUT IP
+#                       b. credentials[1] - string to specify the username to ssh to DUT
+#                       c. credentials[2] - string to specify the password to ssh to DUT
+#                3. command - string to specify the command to be executed in DUT
+# Return Value : console output of the command executed on DUT
+#-------------------------------------------------------------------
+def rdkvapparmor_executeInDUT (sshMethod, credentials, command):
+    output = ""
+    if sshMethod == "directSSH":
+        credentialsList = credentials.split(',')
+        host_name = credentialsList[0]
+        user_name = credentialsList[1]
+        password = credentialsList[2]
+    else:
+        #TODO
+        print "Secure ssh to CPE"
+        pass
+    try:
+        output = ssh_and_execute (sshMethod, host_name, user_name, password, command)
+    except Exception as e:
+        print "Exception occured during ssh session"
+        print e
+    return output
+
+#------------------------------------------------------------------
+# REBOOT THE DEVICE
+#------------------------------------------------------------------
+def rdkvapparmor_rebootDevice(waitTime):
+    status = rdkvapparmor_getPluginStatus("org.rdk.System")
+    timeout = time.time() + 30*5
+    deviceStatus = "DOWN"
+    if status != "activated":
+        print "[INFO] Activating System plugin for rebooting the device"
+        params = '{"callsign":"org.rdk.System"}'
+        status = rdkvapparmor_setValue("Controller.1.activate",params)
+        status = rdkvapparmor_getPluginStatus("org.rdk.System")
+    if status == "activated":
+        print "[INFO] System plugin is activated"
+        data = '"method": "org.rdk.System.1.reboot","params": {"rebootReason": "TDK_TESTING"}'
+        result = execute_step(data)
+        if result != "EXCEPTION OCCURRED":
+            print "\n[INFO] Waiting for the device to come up..."
+            time.sleep(waitTime)
+            while True:
+                status = rdkvapparmor_getTestDeviceStatus()
+                if status == "FREE":
+                    deviceStatus = "UP"
+                    break;
+                elif time.time() > timeout:
+                    deviceStatus = "DOWN"
+                    print "[INFO] Device is not coming up even after 2.5 mins"
+                    break;
+                time.sleep(5)
+            if deviceStatus == "UP":
+                print "[INFO] Device is UP"
+                return "SUCCESS"
+            else:
+                return "FAILURE"
+        else:
+            return result
+    else:
+        print "FAILURE: Unable to activate System plugin"
+        return "EXCEPTION OCCURED"
+
+#------------------------------------------------------------------
+# Check Device Status
+#------------------------------------------------------------------
+def rdkvapparmor_getTestDeviceStatus():
+    try:
+        data = '{"jsonrpc":"2.0","id":"2","method": "Controller.1.status@Controller"}'
+        headers = {'content-type': 'text/plain;'}
+        url = 'http://' + str(deviceIP) + ':' + str(devicePort) + '/jsonrpc'
+        response = requests.post(url, headers=headers, data=data, timeout=3)
+        if response.status_code == 200:
+            jsonResponse = json.loads(response.content,strict=False)
+            return "FREE"
+        else:
+            return "NOT_FOUND"
+    except Exception as e:
+        return "NOT_FOUND"    
+
+#-----------------------------------------------------------------
+# GET PLUGIN STATUS
+#-----------------------------------------------------------------
+def rdkvapparmor_getPluginStatus(plugin):
+    data = '"method": "Controller.1.status@'+plugin+'"'
+    result = execute_step(data)
+    if result != None and result != "EXCEPTION OCCURRED":
+        for x in result:
+            PluginStatus=x["state"]
+        return PluginStatus
+    else:
+        return result;
+		
+#------------------------------------------------------------------
+# SET VALUE FOR A METHOD
+#------------------------------------------------------------------
+def rdkvapparmor_setValue(method,value):
+    data = '"method": "'+method+'","params": '+value
+    result = execute_step(data)
+    return result
+
+#---------------------------------------------------------------
+# EXECUTE CURL REQUESTS
+# Description  : Execute curl request in DUT
+# Parameters   : Data - a string which contains actual curl request
+# Return Value : contains response of the curl request sent
+#---------------------------------------------------------------
+def execute_step(Data):
+    data = '{"jsonrpc": "2.0", "id": 1234567890, '+Data+'}'
+    headers = {'content-type': 'text/plain;',}
+    url = 'http://'+str(deviceIP)+':'+str(devicePort)+'/jsonrpc'
+    try:
+        response = requests.post(url, headers=headers, data=data, timeout=20)
+        json_response = json.loads(response.content)
+        result = json_response.get("result")
+        if result != None and "'success': False" in str(result):
+            result = "EXCEPTION OCCURRED"
+        return result;
+    except requests.exceptions.RequestException as e:
+        print "ERROR!! \nEXCEPTION OCCURRED WHILE EXECUTING CURL COMMANDS!!"
+        print "Error message received :\n",e;
+        return "EXCEPTION OCCURRED"
+
---
+++ framework/web-app/fileStore/testscriptsRDKV/component/rdkv_apparmor/RDKV_AppArmor_Complain.py
@@ -0,0 +1,263 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2023 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>RDKV_AppArmor_Complain</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>rdkvapparmor_executeInDUT</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To test if apparmor is in complain mode for the given profiles</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>5</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>RPI-Client</box_type>
+    <!--  -->
+    <box_type>RPI-HYB</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>RDKV_APPARMOR_02</test_case_id>
+    <test_objective>To test if apparmor is in complain mode for the given profiles</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>RPI</test_setup>
+    <pre_requisite>1. Configure the values AppArmor_Profiles available in fileStore/ApparmorConfig.config file</pre_requisite>
+    <api_or_interface_used></api_or_interface_used>
+    <input_parameters>AppArmor_Profiles</input_parameters>
+    <automation_approch>
+1. Check if AppArmor is supported in device
+2. Complain AppArmor to the given profiles using tr181 command
+3. Validate if the AppArmor is complain to the given profiles</automation_approch>
+    <expected_output>AppArmor should be in complained mode in the given profiles</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>rdkv_apparmor</test_stub_interface>
+    <test_script>RDKV_AppArmor_Complain</test_script>
+    <skipped>No</skipped>
+    <release_version>M109</release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib; 
+from rdkv_apparmorlib import *
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("rdkv_apparmor","1",standAlone=True);
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'RDKV_AppArmor_Complain');
+
+#Get the result of connection with test component and DUT
+result =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %result;
+obj.setLoadModuleStatus(result)
+
+expectedResult = "SUCCESS"
+if expectedResult in result.upper():
+    print "Retrieving Configuration values from config file......."
+    configKeyList = ["AppArmor_Profiles", "SSH_METHOD", "SSH_USERNAME", "SSH_PASSWORD"]
+    configValues = obtainCredentials(obj,configKeyList)
+    AppArmor_Profiles = configValues["AppArmor_Profiles"]
+    credentials = obj.IP + ',' + configValues["SSH_USERNAME"] + ',' + configValues["SSH_PASSWORD"]
+    print "\nTo Ensure Apparmor service is running"
+    command = 'systemctl status apparmor.service | grep active | grep -v inactive'
+    print "COMMAND : %s" %(command)
+
+    #Primitive test case which associated to this Script
+    tdkTestObj = obj.createTestStep('rdkvapparmor_executeInDUT');
+    #Add the parameters to ssh to the DUT and execute the command
+    tdkTestObj.addParameter("sshMethod", configValues["SSH_METHOD"]);
+    tdkTestObj.addParameter("credentials", credentials);
+    tdkTestObj.addParameter("command", command);
+
+    #Execute the test case in DUT
+    tdkTestObj.executeTestCase(expectedResult);
+    result = tdkTestObj.getResult()
+
+    #Get the result of execution
+    output = tdkTestObj.getResultDetails();
+    if "Active: active" in output and expectedResult in result:
+        print "Apparmor is running %s" %(output)
+        
+        #To check Apparmor is enabled
+        command = 'aa-enabled'
+        print "COMMAND : %s" %(command)
+        #Primitive test case which associated to this Script
+        tdkTestObj = obj.createTestStep('rdkvapparmor_executeInDUT');
+        #Add the parameters to ssh to the DUT and execute the command
+        tdkTestObj.addParameter("sshMethod", configValues["SSH_METHOD"]);
+        tdkTestObj.addParameter("credentials", credentials);
+        tdkTestObj.addParameter("command", command);
+
+        #Execute the test case in DUT
+        tdkTestObj.executeTestCase(expectedResult);
+        result = tdkTestObj.getResult()
+
+        #Get the result of execution
+        output = tdkTestObj.getResultDetails();
+        if 'Yes' in output and expectedResult in result:
+            print "Apparmor is enabled"
+            
+            #Set Apparmor profile mode into complain mode with tr181 command
+            print (AppArmor_Profiles)
+            AppArmor_Profiles=AppArmor_Profiles.split(',')
+            count = len(AppArmor_Profiles)
+            print(count)
+            for i in range(count):
+                command = 'tr181 -s -v "'+AppArmor_Profiles[i]+':complain" Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.NonRootSupport.ApparmorBlocklist'
+                print "COMMAND : %s" %(command)
+                #Primitive test case which associated to this Script
+                tdkTestObj = obj.createTestStep('rdkvapparmor_executeInDUT');
+                #Add the parameters to ssh to the DUT and execute the command
+                tdkTestObj.addParameter("sshMethod", configValues["SSH_METHOD"]);
+                tdkTestObj.addParameter("credentials", credentials);
+                tdkTestObj.addParameter("command", command);
+
+                #Execute the test case in DUT
+                tdkTestObj.executeTestCase(expectedResult);
+                result = tdkTestObj.getResult()
+            if expectedResult in result:
+                print "tr181 is set"
+                #Reboot the device
+                tdkTestObj = obj.createTestStep('rdkvapparmor_rebootDevice')
+                tdkTestObj.addParameter("waitTime",50)
+                #Execute the test case in DUT
+                tdkTestObj.executeTestCase(expectedResult);
+                result = tdkTestObj.getResult()
+                if expectedResult in result:
+                    print "Device is rebooted"
+                    
+                    #check status from /opt/secure/Apparmor_blocklist
+                    command = 'cat /opt/secure/Apparmor_blocklist'
+                    print "COMMAND : %s" %(command)
+                    #Primitive test case which associated to this Script
+                    tdkTestObj = obj.createTestStep('rdkvapparmor_executeInDUT');
+                    #Add the parameters to ssh to the DUT and execute the command
+                    tdkTestObj.addParameter("sshMethod", configValues["SSH_METHOD"]);
+                    tdkTestObj.addParameter("credentials", credentials);
+                    tdkTestObj.addParameter("command", command);
+
+                    #Execute the test case in DUT
+                    tdkTestObj.executeTestCase(expectedResult);
+                    result = tdkTestObj.getResult()
+                    output = tdkTestObj.getResultDetails()
+                    for i in range(count):
+                        if ''+AppArmor_Profiles[i]+':complain' in output:
+                            complain = "Success"
+                        else:
+                            complain = "Failure"
+                    if complain == "Success":
+                        print "Status is valid in Apparmor_blocklist"
+
+                        #To check if profiles are loaded into kernel
+                        command = 'cat /sys/kernel/security/apparmor/profiles'
+                        print "COMMAND : %s" %(command)
+                        #Primitive test case which associated to this Script
+                        tdkTestObj = obj.createTestStep('rdkvapparmor_executeInDUT');
+                        #Add the parameters to ssh to the DUT and execute the command
+                        tdkTestObj.addParameter("sshMethod", configValues["SSH_METHOD"]);
+                        tdkTestObj.addParameter("credentials", credentials);
+                        tdkTestObj.addParameter("command", command);
+
+                        #Execute the test case in DUT
+                        tdkTestObj.executeTestCase(expectedResult);
+                        result = tdkTestObj.getResult()
+                        output = tdkTestObj.getResultDetails()
+                        for i in range(count):
+                            if ''+AppArmor_Profiles[i]+' (complain)' in output:
+                                kernel = "Success"
+                            else:
+                                kernel = "Failure"
+                        if kernel == "Success":
+                            print "apparmor profiles are loaded in to kernel space"
+
+                            #Check for AppArmor initialization logs
+                            command = 'cat /opt/logs/startup_stdout_log.txt | grep "Starting AppArmor initialization"'
+                            print "COMMAND : %s" %(command)
+                            #Primitive test case which associated to this Script
+                            tdkTestObj = obj.createTestStep('rdkvapparmor_executeInDUT');
+                            #Add the parameters to ssh to the DUT and execute the command
+                            tdkTestObj.addParameter("sshMethod", configValues["SSH_METHOD"]);
+                            tdkTestObj.addParameter("credentials", credentials);
+                            tdkTestObj.addParameter("command", command);
+
+                            #Execute the test case in DUT
+                            tdkTestObj.executeTestCase(expectedResult);
+                            result = tdkTestObj.getResult()
+                            output = tdkTestObj.getResultDetails()
+                            if "Starting AppArmor initialization" in output:
+                                print "AppArmor initialized successfully"
+                            else:
+                                print "Unable to get the required logs"
+                                tdkTestObj.setResultStatus("FAILURE")
+                        else:
+                            print "apparmor profiles are not loaded in to kernel space"
+                            tdkTestObj.setResultStatus("FAILURE")
+                    else:
+                        print "Status is invalid in Apparmor_blocklist"
+                        tdkTestObj.setResultStatus("FAILURE")
+                else:
+                    print "Device is not rebooted"
+                    tdkTestObj.setResultStatus("FAILURE")
+            else:
+                print "tr181 is not set"
+                tdkTestObj.setResultStatus("FAILURE")
+        else:
+            print "Apparmor is not enabled"
+            tdkTestObj.setResultStatus("FAILURE")
+    else:
+        print "AppArmor is not supported"
+        tdkTestObj.setResultStatus("FAILURE")
+
+    #Unload the module
+    obj.unloadModule("rdkv_apparmor");
+else:
+    #Set load module status
+    obj.setLoadModuleStatus("FAILURE");
+    print "FAILURE: Failed to load module"
+
