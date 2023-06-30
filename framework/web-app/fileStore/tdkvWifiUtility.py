dbbf5634a445ce4df6dd39322e0f2e7a3e4f3075
AbhishekMurali545 <abhishek_murali@comcast.com>
2023-06-30 06:42:15 +0000

    RDKTT-4266: Enhance HAL framework
    Reason for change: Add device specific file provision for bluetoothhal
    Enhance wifihal config file by removing the addition of <device_name.config>
    Test Procedure: Verify with TDK TM
    Risks: None
    Priority: P1
    Signed-off-by: Abhishek Murali <abhishek_murali@comcast.com>
    
    (cherry picked from commit abf0db5682669bfc7011e5eb50b2c14f536ba74f)
    Change-Id: Ia1fcd3606899ae89c9940c2647bed449970f97c7

--- framework/web-app/fileStore/tdkvWifiUtility.py
+++ framework/web-app/fileStore/tdkvWifiUtility.py
@@ -49,30 +49,31 @@ def parseDeviceConfig(obj):
         deviceDetails = obj.getDeviceDetails()
         deviceName = deviceDetails["devicename"]
         #Get the device configuration file name
-        deviceConfig = deviceName + ".config"
+        device_Config = deviceName + ".config"
 
         #Get the current directory path
         configFilePath = os.path.dirname(os.path.realpath(__file__))
         configFilePath = configFilePath + "/tdkvDeviceConfig"
 
-        print "Device config file:", configFilePath+'/'+deviceConfig
+        print "Device config file:", configFilePath+'/'+device_Config
 
         #Parse the device configuration file
         config = ConfigParser.ConfigParser()
-        config.read(configFilePath+'/'+deviceConfig)
+        config.read(configFilePath+'/'+device_Config)
 
         #Parse the file and store the values in global variables
         global setup_type
-        setup_type = config.get(deviceConfig, 'SETUP_TYPE')
+        wifi_config = "wifi-config"
+        setup_type = config.get(wifi_config, 'SETUP_TYPE')
 
         global ssid_2ghz_name
-        ssid_2ghz_name = config.get(deviceConfig, "SSID_2GHZ_NAME")
+        ssid_2ghz_name = config.get(wifi_config, "SSID_2GHZ_NAME")
 
         global ssid_2ghz_pwd
-        ssid_2ghz_pwd = config.get(deviceConfig, "SSID_2GHZ_PWD")
+        ssid_2ghz_pwd = config.get(wifi_config, "SSID_2GHZ_PWD")
 
         global ssid_2ghz_invalid_pwd
-        ssid_2ghz_invalid_pwd = config.get(deviceConfig, "SSID_2GHZ_INVALID_PWD")
+        ssid_2ghz_invalid_pwd = config.get(wifi_config, "SSID_2GHZ_INVALID_PWD")
 
         global ssid_2ghz_index
         global radio_2ghz_index
@@ -80,107 +81,107 @@ def parseDeviceConfig(obj):
         global radio_5ghz_index
 
         if setup_type == "TDK":
-            ssid_2ghz_index = config.get(deviceConfig, "TDK_SSID_2GHZ_INDEX")
-            radio_2ghz_index = config.get(deviceConfig, "TDK_RADIO_2GHZ_INDEX")
-            ssid_5ghz_index = config.get(deviceConfig, "TDK_SSID_5GHZ_INDEX")
-            radio_5ghz_index = config.get(deviceConfig, "TDK_RADIO_5GHZ_INDEX")
+            ssid_2ghz_index = config.get(wifi_config, "TDK_SSID_2GHZ_INDEX")
+            radio_2ghz_index = config.get(wifi_config, "TDK_RADIO_2GHZ_INDEX")
+            ssid_5ghz_index = config.get(wifi_config, "TDK_SSID_5GHZ_INDEX")
+            radio_5ghz_index = config.get(wifi_config, "TDK_RADIO_5GHZ_INDEX")
         else:
             print "SETUP TYPE IS NOT VALID";
         global ssid_5ghz_name
-        ssid_5ghz_name = config.get(deviceConfig, "SSID_5GHZ_NAME")
+        ssid_5ghz_name = config.get(wifi_config, "SSID_5GHZ_NAME")
 
         global ssid_5ghz_pwd
-        ssid_5ghz_pwd = config.get(deviceConfig, "SSID_5GHZ_PWD")
+        ssid_5ghz_pwd = config.get(wifi_config, "SSID_5GHZ_PWD")
 
         global ssid_5ghz_invalid_pwd
-        ssid_5ghz_invalid_pwd = config.get(deviceConfig, "SSID_5GHZ_INVALID_PWD")
+        ssid_5ghz_invalid_pwd = config.get(wifi_config, "SSID_5GHZ_INVALID_PWD")
 
         global wlan_2ghz_ssid_connect_status
-        wlan_2ghz_ssid_connect_status = config.get(deviceConfig, "WLAN_2GHZ_SSID_CONNECT_STATUS")
+        wlan_2ghz_ssid_connect_status = config.get(wifi_config, "WLAN_2GHZ_SSID_CONNECT_STATUS")
 
         global wlan_5ghz_ssid_connect_status
-        wlan_5ghz_ssid_connect_status = config.get(deviceConfig, "WLAN_5GHZ_SSID_CONNECT_STATUS")
+        wlan_5ghz_ssid_connect_status = config.get(wifi_config, "WLAN_5GHZ_SSID_CONNECT_STATUS")
 
         global wlan_2ghz_ssid_disconnect_status
-        wlan_2ghz_ssid_disconnect_status = config.get(deviceConfig, "WLAN_2GHZ_SSID_DISCONNECT_STATUS")
+        wlan_2ghz_ssid_disconnect_status = config.get(wifi_config, "WLAN_2GHZ_SSID_DISCONNECT_STATUS")
 
         global wlan_5ghz_ssid_disconnect_status
-        wlan_5ghz_ssid_disconnect_status = config.get(deviceConfig, "WLAN_5GHZ_SSID_DISCONNECT_STATUS")
+        wlan_5ghz_ssid_disconnect_status = config.get(wifi_config, "WLAN_5GHZ_SSID_DISCONNECT_STATUS")
 
         global ssid_invalid_name
-        ssid_invalid_name = config.get(deviceConfig, "SSID_INVALID_NAME")
+        ssid_invalid_name = config.get(wifi_config, "SSID_INVALID_NAME")
 
         global ssid_invalid_pwd
-        ssid_invalid_pwd = config.get(deviceConfig, "SSID_INVALID_PWD")
+        ssid_invalid_pwd = config.get(wifi_config, "SSID_INVALID_PWD")
 
         global ap_5ghz_preshared_key
-	ap_5ghz_preshared_key = config.get(deviceConfig, "AP_5GHZ_PRESHARED_KEY")
+	ap_5ghz_preshared_key = config.get(wifi_config, "AP_5GHZ_PRESHARED_KEY")
 
         global ap_2ghz_preshared_key
-	ap_2ghz_preshared_key = config.get(deviceConfig, "AP_2GHZ_PRESHARED_KEY")
+	ap_2ghz_preshared_key = config.get(wifi_config, "AP_2GHZ_PRESHARED_KEY")
 
 	global ap_5ghz_key_passphrase
-	ap_5ghz_key_passphrase = config.get(deviceConfig, "AP_5GHZ_KEY_PASSPHRASE")
+	ap_5ghz_key_passphrase = config.get(wifi_config, "AP_5GHZ_KEY_PASSPHRASE")
 
 	global ap_2ghz_key_passphrase
-	ap_2ghz_key_passphrase = config.get(deviceConfig, "AP_2GHZ_KEY_PASSPHRASE")
+	ap_2ghz_key_passphrase = config.get(wifi_config, "AP_2GHZ_KEY_PASSPHRASE")
 
 	global ap_5ghz_security_mode 
-	ap_5ghz_security_mode = config.get(deviceConfig, "AP_5GHZ_SECURITY_MODE")
+	ap_5ghz_security_mode = config.get(wifi_config, "AP_5GHZ_SECURITY_MODE")
 
 	global ap_2ghz_security_mode 
-	ap_2ghz_security_mode = config.get(deviceConfig, "AP_2GHZ_SECURITY_MODE")
+	ap_2ghz_security_mode = config.get(wifi_config, "AP_2GHZ_SECURITY_MODE")
 
 	global ap_5ghz_wep_key
-	ap_5ghz_wep_key = config.get(deviceConfig, "AP_5GHZ_WEP_KEY")
+	ap_5ghz_wep_key = config.get(wifi_config, "AP_5GHZ_WEP_KEY")
 
 	global ap_2ghz_wep_key
-	ap_2ghz_wep_key = config.get(deviceConfig, "AP_2GHZ_WEP_KEY")
+	ap_2ghz_wep_key = config.get(wifi_config, "AP_2GHZ_WEP_KEY")
 
         global ssid_5ghz_name_new
-        ssid_5ghz_name_new = config.get(deviceConfig, "SSID_5GHZ_NAME_NEW")
+        ssid_5ghz_name_new = config.get(wifi_config, "SSID_5GHZ_NAME_NEW")
 
         global ssid_2ghz_name_new
-        ssid_2ghz_name_new = config.get(deviceConfig, "SSID_2GHZ_NAME_NEW")
+        ssid_2ghz_name_new = config.get(wifi_config, "SSID_2GHZ_NAME_NEW")
 
         global ssid_5ghz_pwd_new
-        ssid_5ghz_pwd_new = config.get(deviceConfig, "SSID_5GHZ_PWD_NEW")
+        ssid_5ghz_pwd_new = config.get(wifi_config, "SSID_5GHZ_PWD_NEW")
 
         global ssid_2ghz_pwd_new
-        ssid_2ghz_pwd_new = config.get(deviceConfig, "SSID_2GHZ_PWD_NEW")
+        ssid_2ghz_pwd_new = config.get(wifi_config, "SSID_2GHZ_PWD_NEW")
 
         global ap_5ghz_preshared_key_new
-        ap_5ghz_preshared_key_new = config.get(deviceConfig, "AP_5GHZ_PRESHARED_KEY_NEW")
+        ap_5ghz_preshared_key_new = config.get(wifi_config, "AP_5GHZ_PRESHARED_KEY_NEW")
 
         global ap_2ghz_preshared_key_new
-        ap_2ghz_preshared_key_new = config.get(deviceConfig, "AP_2GHZ_PRESHARED_KEY_NEW")
+        ap_2ghz_preshared_key_new = config.get(wifi_config, "AP_2GHZ_PRESHARED_KEY_NEW")
 
         global ap_5ghz_key_passphrase_new
-        ap_5ghz_key_passphrase_new = config.get(deviceConfig, "AP_5GHZ_KEY_PASSPHRASE_NEW")
+        ap_5ghz_key_passphrase_new = config.get(wifi_config, "AP_5GHZ_KEY_PASSPHRASE_NEW")
 
         global ap_2ghz_key_passphrase_new
-        ap_2ghz_key_passphrase_new = config.get(deviceConfig, "AP_2GHZ_KEY_PASSPHRASE_NEW")
+        ap_2ghz_key_passphrase_new = config.get(wifi_config, "AP_2GHZ_KEY_PASSPHRASE_NEW")
 
         global ap_5ghz_security_mode_new
-        ap_5ghz_security_mode_new = config.get(deviceConfig, "AP_5GHZ_SECURITY_MODE_NEW")
+        ap_5ghz_security_mode_new = config.get(wifi_config, "AP_5GHZ_SECURITY_MODE_NEW")
 
         global ap_2ghz_security_mode_new
-        ap_2ghz_security_mode_new = config.get(deviceConfig, "AP_2GHZ_SECURITY_MODE_NEW")
+        ap_2ghz_security_mode_new = config.get(wifi_config, "AP_2GHZ_SECURITY_MODE_NEW")
 
         global ap_5ghz_wep_key_new
-        ap_5ghz_wep_key_new = config.get(deviceConfig, "AP_5GHZ_WEP_KEY_NEW")
+        ap_5ghz_wep_key_new = config.get(wifi_config, "AP_5GHZ_WEP_KEY_NEW")
 
         global ap_2ghz_wep_key_new
-        ap_2ghz_wep_key_new = config.get(deviceConfig, "AP_2GHZ_WEP_KEY_NEW")
+        ap_2ghz_wep_key_new = config.get(wifi_config, "AP_2GHZ_WEP_KEY_NEW")
 
 	global wifi_interface
-	wifi_interface = config.get(deviceConfig, "WIFI_INTERFACE")
+	wifi_interface = config.get(wifi_config, "WIFI_INTERFACE")
 
         global set_5ghz_freq_list
-        set_5ghz_freq_list = config.get(deviceConfig, "SET_5GHZ_FREQ_LIST")
+        set_5ghz_freq_list = config.get(wifi_config, "SET_5GHZ_FREQ_LIST")
 
         global set_2ghz_freq_list
-        set_2ghz_freq_list = config.get(deviceConfig, "SET_2GHZ_FREQ_LIST")
+        set_2ghz_freq_list = config.get(wifi_config, "SET_2GHZ_FREQ_LIST")
 
     except Exception, e:
         print e;
