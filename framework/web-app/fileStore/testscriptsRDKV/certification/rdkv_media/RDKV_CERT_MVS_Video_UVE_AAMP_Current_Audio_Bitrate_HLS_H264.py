7ecd6f4dbdbd53076cfb92e4059fc9c02643e5f1
ssimon420 <shinu_simon@comcast.com>
2023-07-03 16:08:46 +0530

    RDKTT-4267 : Add new audio bitrate script
    Add new audio bitrate script for hls stream
    Reason for change: Enhance TDK scripts
    Test Procedure: Check if test success with TDK
    Risks: None
    Signed-off-by:Shinu S<shinu.s@tataelxsi.co.in>
    
    Signed-off-by: ssimon420 <shinu_simon@comcast.com>
    (cherry picked from commit 801f054dad3936ff371f56ddb9aa3245b0f83e93)
    Change-Id: I8e8070184ddc734ea53e1f5d5aa40dda5952eb9f

--- framework/web-app/fileStore/testscriptsRDKV/certification/rdkv_media/RDKV_CERT_MVS_Video_UVE_AAMP_Current_Audio_Bitrate_HLS_H264.py
+++ framework/web-app/fileStore/testscriptsRDKV/certification/rdkv_media/RDKV_CERT_MVS_Video_UVE_AAMP_Current_Audio_Bitrate_HLS_H264.py
@@ -23,7 +23,7 @@
   <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
   <version>1</version>
   <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
-  <name>RDKV_CERT_MVS_Video_UVE_AAMP_Current_Audio_Bitrate</name>
+  <name>RDKV_CERT_MVS_Video_UVE_AAMP_Current_Audio_Bitrate_HLS_H264</name>
   <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
   <primitive_test_id></primitive_test_id>
   <!-- Do not change primitive_test_id if you are editing an existing script. -->
@@ -33,7 +33,7 @@
   <!--  -->
   <status>FREE</status>
   <!--  -->
-  <synopsis>Test Script to launch a lightning UVE player application via Webkit instance and perform video play operation of H.264 codec dash stream for few minutes, get the current audio bitrate and close the player</synopsis>
+  <synopsis>Test Script to launch a lightning UVE player application via Webkit instance and perform video play operation of H.264 codec hls stream for few minutes, get the current audio bitrate and close the player</synopsis>
   <!--  -->
   <groups_id />
   <!--  -->
@@ -48,13 +48,13 @@
   <skip>false</skip>
   <!--  -->
   <box_types>
-    <box_type>RPI-HYB</box_type>
+    <box_type>RDKTV</box_type>
     <!--  -->
     <box_type>RPI-Client</box_type>
     <!--  -->
-    <box_type>Video_Accelerator</box_type>
+    <box_type>RPI-HYB</box_type>
     <!--  -->
-    <box_type>RDKTV</box_type>
+    <box_type>Video_Accelerator</box_type>
     <!--  -->
   </box_types>
   <rdk_versions>
@@ -62,8 +62,8 @@
     <!--  -->
   </rdk_versions>
   <test_cases>
-    <test_case_id>RDKV_Media_Validation_309</test_case_id>
-    <test_objective>Test Script to launch a lightning UVE player application via Webkit instance and perform video play operation of H.264 codec dash stream for few minutes,get current audio bitrate and close the player</test_objective>
+    <test_case_id>RDKV_Media_Validation_330</test_case_id>
+    <test_objective>Test Script to launch a lightning UVE player application via Webkit instance and perform video play operation of H.264 codec hls stream for few minutes,get current audio bitrate and close the player</test_objective>
     <test_type>Positive</test_type>
     <test_setup>RDKTV,RPI, Accelerator</test_setup>
     <pre_requisite>1. Wpeframework process should be up and running in the device.
@@ -72,21 +72,20 @@
     <input_parameters>Lightning UVE player App URL: string
 webkit_instance:string
 webinspect_port: string
-video_src_url_dash_h264: string
-</input_parameters>
+video_src_url_hls_h264: string</input_parameters>
     <automation_approch>1. As pre requisite, launch webkit instance via RDKShell, open websocket conntion to webinspect page
 2. Store the details of other launched apps. Move the webkit instance to front, if its z-order is low.
 3. Launch webkit instance with uve test app with the video src url and duration for close.
-4. App starts playing the h.264 dash stream video, gets the current audio bitrate and closes the player after the provided duration.
+4. App starts playing the h.264 hls stream video, gets the current audio bitrate and closes the player after the provided duration.
 5. If expected event video playing is observed then update the result as SUCCESS or else FAILURE
 6. Update the test script result as SUCCESS/FAILURE based on event validation result and proc check status (if applicable)
 7. Revert all values</automation_approch>
     <expected_output>UVE Player should play the video for provided duration, expected event playing should occur, fetch the current audio bitrate and if proc validation is applicable, then expected data should be available in proc file </expected_output>
     <priority>High</priority>
     <test_stub_interface>rdkv_media</test_stub_interface>
-    <test_script>RDKV_CERT_MVS_Video_UVE_AAMP_Current_Audio_Bitrate</test_script>
+    <test_script>RDKV_CERT_MVS_Video_UVE_AAMP_Current_Audio_Bitrate_HLS_H264</test_script>
     <skipped>No</skipped>
-    <release_version>M112</release_version>
+    <release_version>M114</release_version>
     <remarks></remarks>
   </test_cases>
 </xml>
@@ -103,7 +102,7 @@ obj = tdklib.TDKScriptingLibrary("rdkv_media","1",standAlone=True)
 #This will be replaced with corresponding DUT Ip and port while executing script
 ip = <ipaddress>
 port = <port>
-obj.configureTestCase(ip,port,'RDKV_CERT_MVS_Video_UVE_AAMP_Current_Audio_Bitrate')
+obj.configureTestCase(ip,port,'RDKV_CERT_MVS_Video_UVE_AAMP_Current_Audio_Bitrate_HLS_H264')
 
 webkit_console_socket = None
 
@@ -129,7 +128,7 @@ if expectedResult in result.upper():
         conf_file,result = getDeviceConfigFile(obj.realpath)
         setDeviceConfigFile(conf_file)
         appURL    = MediaValidationVariables.lightning_uve_test_app_url
-        videoURL  = MediaValidationVariables.video_src_url_dash_h264
+        videoURL  = MediaValidationVariables.video_src_url_hls_h264
         # Setting VideoPlayer Operations
         setOperation("curraudiobitrate","20")
         operations = getOperations()
