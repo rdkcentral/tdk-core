63669c63ae90b4e325f8efefa386ff67247c6cb8
ksrika861 <Karthik_Srikanth@comcast.com>
2023-07-27 18:00:42 +0530

    RDKTT-4281: Basic sanity Reboot Test cases - Master shell script Activity
    Reason for change: Changed existing reboot involved shell scripts - system_sanity_check_before_reboot.sh and system_sanity_check_after_reboot.sh to run with arguments and seperate py scripts to each test cases.
    Test Procedure: Run test script and check if test is successful and displays results
    Risks: None
    Priority: P2
    Signed-off-by: Karthik_Srikanth@comcast.com
    Change-Id: I4f37648a5701c73cd7682f4636b5018e44b67e97

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/rdkv_basic_sanity/RDKV_Basic_Sanity_Plugin_Status_Before_After_Reboot.py
@@ -0,0 +1,161 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2023 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>1</version>
+  <name>RDKV_Basic_Sanity_Plugin_Status_Before_After_Reboot</name>
+  <primitive_test_id/>
+  <primitive_test_name>rdkv_basic_sanity_executeInDUT</primitive_test_name>
+  <primitive_test_version>1</primitive_test_version>
+  <status>FREE</status>
+  <synopsis>To validate Status of Plugins before and after reboot</synopsis>
+  <groups_id/>
+  <execution_time>10</execution_time>
+  <long_duration>false</long_duration>
+  <advanced_script>false</advanced_script>
+  <remarks/>
+  <skip>false</skip>
+  <box_types>
+    <box_type>RPI-Client</box_type>
+    <box_type>RPI-HYB</box_type>
+    <box_type>Video_Accelerator</box_type>
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>RDKV_Basic_Sanity_04</test_case_id>
+    <test_objective>To Initiate the shell scripts to validate Status of Plugins before and after reboot</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>RPI-HYB,RPI-Client,Video_Accelerator</test_setup>
+    <pre_requisite>1. Configure the location of shell scripts in FilePath available in fileStore/Basic_Sanity_Config.config file</pre_requisite>
+    <api_or_interface_used>None</api_or_interface_used>
+    <input_parameters>FilePath</input_parameters>
+    <automation_approch>Find the plugins which has autostart:true and validate Status of Plugins before and after reboot</automation_approch>
+    <expected_output>Respective Scripts are executed where reboot is involved and results are available in Logfile,  SUCCESS if plugin status are same before and after reboot.</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>rdkv_basic_sanity</test_stub_interface>
+    <test_script>RDKV_Basic_Sanity_Plugin_Status_Before_After_Reboot</test_script>
+    <skipped>No</skipped>
+    <release_version>M115</release_version>
+    <remarks/>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+from rdkv_basic_sanitylib import *
+import time
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("rdkv_basic_sanity","1",standAlone=True);
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'RDKV_Basic_Sanity_Plugin_Status_Before_After_Reboot');
+
+#Get the result of connection with test component and DUT
+result =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %result;
+obj.setLoadModuleStatus(result.upper());
+
+expectedResult = "SUCCESS"
+if expectedResult in result.upper():
+    configKeyList = ["FilePath","SSH_METHOD", "SSH_USERNAME", "SSH_PASSWORD"]
+    configValues={}
+    tdkTestObj = obj.createTestStep('rdkv_basic_sanity_getDeviceConfig')
+    #Get each configuration from device config file
+    for configKey in configKeyList:
+        tdkTestObj.addParameter("basePath",obj.realpath)
+        tdkTestObj.addParameter("configKey",configKey)
+        tdkTestObj.executeTestCase(expectedResult)
+        configValues[configKey] = tdkTestObj.getResultDetails()
+        if "FAILURE" not in configValues[configKey] and configValues[configKey] != "":
+            print "SUCCESS: Successfully retrieved %s configuration from device config file" %(configKey)
+            tdkTestObj.setResultStatus("SUCCESS")
+        else:
+            print "FAILURE: Failed to retrieve %s configuration from device config file" %(configKey)
+            if configValues[configKey] == "":
+                print "\n Please configure the %s key in the device config file" %(configKey)
+            tdkTestObj.setResultStatus("FAILURE")
+            result = "FAILURE"
+            break
+    if "FAILURE" != result:
+        if "directSSH" == configValues["SSH_METHOD"] :
+            if configValues["SSH_PASSWORD"] == "None":
+                configValues["SSH_PASSWORD"] = ""
+            credentials = obj.IP + ',' + configValues["SSH_USERNAME"] + ',' + configValues["SSH_PASSWORD"]
+            command = "sh " + configValues["FilePath"] + "/system_sanity_check_before_reboot.sh 1"
+            print("COMMAND: %s" % command)
+            #Primitive test case associated with this Script
+            tdkTestObj = obj.createTestStep('rdkv_basic_sanity_rebootexecution');
+            #Add the parameters to ssh to the DUT and execute the command
+            tdkTestObj.addParameter("sshMethod", configValues["SSH_METHOD"]);
+            tdkTestObj.addParameter("credentials", credentials);
+            tdkTestObj.addParameter("command", command);
+            tdkTestObj.executeTestCase(expectedResult);
+            result = tdkTestObj.getResult();
+            output = tdkTestObj.getResultDetails();
+            # View log file after 180 seconds
+            print("Waiting for 180 seconds before checking log file to finish reboot process...")
+            time.sleep(180)
+            command = "cat " + configValues["FilePath"] + "/sanity_test_reboot_testcase.log"
+            print("COMMAND: %s" % command)
+            # Primitive test case which associated to this Script
+            tdkTestObj = obj.createTestStep('rdkv_basic_sanity_executeInDUT');
+            tdkTestObj.addParameter("sshMethod", configValues["SSH_METHOD"]);
+            tdkTestObj.addParameter("credentials", credentials);
+            tdkTestObj.addParameter("command", command);
+            tdkTestObj.executeTestCase(expectedResult);
+            result = tdkTestObj.getResult()
+            # Get the result of execution
+            output = tdkTestObj.getResultDetails();
+            output = str(output)
+            print("[RESPONSE FROM DEVICE]: %s" % output)
+            if "FAILURE" in output or expectedResult not in output:
+                #Check if the file exists or not
+                if "No such file or directory" in output:
+                  print "FAILURE: File not found"
+                  tdkTestObj.setResultStatus("FAILURE")
+                else:
+                  print "FAILURE: Script Execution was not Successful"
+                  tdkTestObj.setResultStatus("FAILURE")
+            elif "FAILURE" not in output and expectedResult in output:
+                print "SUCCESS: Script Execution Successful"
+                tdkTestObj.setResultStatus("SUCCESS")
+            else:
+                print "Error: Error in the Script Execution"
+                tdkTestObj.setResultStatus("FAILURE")                                              
+        else:
+            print "FAILURE: Currently only supports directSSH ssh method"
+            tdkTestObj.setResultStatus("FAILURE");
+    else:
+        print "FAILURE: Failed to get configuration values"
+        tdkTestObj.setResultStatus("FAILURE");
+    #Unload the module
+    obj.unloadModule("rdkv_basic_sanity_executeInDUT");
+
+else:
+    #Set load module status
+    obj.setLoadModuleStatus("FAILURE");
+    print "FAILURE: Failed to load module"
+
