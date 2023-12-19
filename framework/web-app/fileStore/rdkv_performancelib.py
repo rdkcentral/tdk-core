eeeae17bc5ed72d3ea6c0a987650df409bd8bc0b
ejose939 <Elizabeth_Jose2@comcast.com>
2023-12-19 16:37:04 +0000

    RDKTT-4390: Fix performance issue
    Reason for change: TypeError is coming when execute suites after
    enabling RDK certification Performance option in TM UI.
    Test Procedure: Run test file and check if test is succesful with TDK
    Risks: None
    Priority: P1
    Signed-off-by: Elizabeth Jose elizabeth_jose2@comcast.com
    
    (cherry picked from commit d2afc30e19cd5dd090c2b85a69236aae6f9bd09a)
    Change-Id: If4ec2a2c5577e09c83d73f6784d0ebef1712d39a

--- framework/web-app/fileStore/rdkv_performancelib.py
+++ framework/web-app/fileStore/rdkv_performancelib.py
@@ -150,8 +150,8 @@ def execute_step(Data,IsPerformanceSelected="false"):
 		return time_taken;
             IsPerformanceSelected = libObj.parentTestCase.performanceBenchMarkingEnabled
             if IsPerformanceSelected == "true":
-                conf_file,result = getConfigFileName(libObj.realpath)
-                result, max_response_time = getDeviceConfigKeyValue(conf_file,"MAX_RESPONSE_TIME")
+                conf_file,result_conf = getConfigFileName(libObj.realpath)
+                result_time, max_response_time = getDeviceConfigKeyValue(conf_file,"MAX_RESPONSE_TIME")
                 time_taken = response.elapsed.total_seconds()
                 print "Time Taken for",Data,"is :", time_taken
                 if (float(time_taken) <= 0 or float(time_taken) > float(max_response_time)):
