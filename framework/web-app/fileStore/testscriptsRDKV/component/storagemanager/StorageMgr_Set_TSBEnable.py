fe063d91efac1798ede202866b56ec066afd27bf
PooraniK <Poorani_k@comcast.com>
2024-06-05 07:57:59 +0000

    RDKTT-4485: Storage Manager Stub Development
    Reason for change: Developing new stub for StoragrMgr
    Test Procedure: Verified using TDK TM
    Risks: None
    Priority : Medium
    Signed-off-by: Pooranik <Poorani_k@comcast.com>
    
    (cherry picked from commit f34a6df87ee4fa6f63aac54d833fec7e4476b3b6)
    Change-Id: If663245524ce16a739e8b429213fb426599800dd

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/storagemanager/StorageMgr_Set_TSBEnable.py
@@ -0,0 +1,162 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2024 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>StorageMgr_Set_TSBEnable</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>GetTSBStatus</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>Test Script to set TSB as enable</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>1</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+    <box_type>RDKTV</box_type>
+    <!--  -->
+    <box_type>RPI-Hybrid</box_type>
+    <!--  -->
+    <box_type>RPI-CLIENT</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_StorageMgr_06</test_case_id>
+    <test_objective>Test Script to set TSB as enable</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Video_Accelerator, RDKTV, RPI</test_setup>
+    <pre_requisite></pre_requisite>
+    <api_or_interface_used>setTSBEnabled, isTSBEnabled</api_or_interface_used>
+    <input_parameters>isEnabled = 1</input_parameters>
+    <automation_approch>1.Load storagemanager module.
+2.Invoke isTSBEnabled API and verify TSB is enable or not
+3.If TSB is already enabled Script will success
+4.Else invoke setTSBEnabled and set TSB as enabled</automation_approch>
+    <expected_output>Should return TSB as enabled</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>libstoragemanagerstub.so.0.0.0</test_stub_interface>
+    <test_script>StorageMgr_Set_TSBEnable</test_script>
+    <skipped>No</skipped>
+    <release_version>M125</release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("storagemanager","1");
+isEnabled = 1;
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'StorageMgr_Set_TSBEnable');
+
+
+#Get the result of connection with test component and DUT
+loadModuleStatus = obj.getLoadModuleResult();
+print("[LIB LOAD STATUS]  :  %s" %loadModuleStatus);
+
+if "SUCCESS" in loadModuleStatus.upper():
+    obj.setLoadModuleStatus("SUCCESS");
+    expectedResult="SUCCESS";
+
+    print("\nTEST STEP : Set the TSB Status using rdkStorage_setTSBEnabled API")
+    print("EXPECTED RESULT : Should return TSB enable or not")
+
+    tdkTestObj = obj.createTestStep('IsTSBEnabled');
+    tdkTestObj.executeTestCase(expectedResult);
+    actualResult = tdkTestObj.getResult();
+    details = tdkTestObj.getResultDetails();
+    print("Is TSB Enabled = ",details)
+    
+    if expectedResult in actualResult and "true" in details:
+        print("\n TSB is Already Enabled")
+        print("[TEST EXECUTION RESULT] : SUCCESS")
+    else:
+        tdkTestObj = obj.createTestStep('SetTSBEnabled');
+        print("\nSet TSB as %d" %(isEnabled));
+        tdkTestObj.addParameter("isEnabled",isEnabled);
+        tdkTestObj.executeTestCase(expectedResult);
+        actualResult = tdkTestObj.getResult();
+        details = tdkTestObj.getResultDetails();
+
+        if expectedResult in actualResult:
+            tdkTestObj.setResultStatus("SUCCESS");
+            print("[TEST EXECUTION RESULT] : SUCCESS")
+            print("\nTEST STEP2 : Get the is TSB is enabled or not using rdkStorage_IsTSBEnabled API ")
+            print("EXPECTED OUTPUT : Should get TSB as enabled")
+            
+            tdkTestObj = obj.createTestStep('IsTSBEnabled');
+            tdkTestObj.executeTestCase(expectedResult);
+            actualResult = tdkTestObj.getResult();
+            details = tdkTestObj.getResultDetails();
+            print("Is TSB Enabled = ",details)
+
+            if expectedResult in actualResult and "true" in details: 
+                tdkTestObj.setResultStatus("SUCCESS");
+                print("ACTUAL RESULT : Setting TSB as enable was success")
+                print("[TEST EXECUTION RESULT] : SUCCESS")
+            else:
+                tdkTestObj.setResultStatus("FAILURE");
+                print("Value Returned : ",details)
+                print("ACTUAL RESULt: Setting TSB as enable was failed")
+                print("[TEST EXECUTION RESULT]: FAILURE\n")
+        else:
+            tdkTestObj.setResultStatus("FAILURE");
+            print(details)
+            print("ACTUAL RESULT: StorageMgr_SetTSBEnabled call failed")
+            print("[TEST EXECUTION RESULT] : FAILURE\n")
+
+    obj.unloadModule("storagemanager");
+
+else:
+    print("Load module failed");
+    obj.setLoadModuleStatus("FAILURE");
+
+
