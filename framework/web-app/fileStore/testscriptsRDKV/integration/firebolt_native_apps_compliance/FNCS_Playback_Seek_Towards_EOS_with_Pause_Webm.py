73c9bddde2fe9fddfc82773377c2726a8577f8f2
PooraniK <Poorani_k@comcast.com>
2024-06-06 10:30:24 +0000

    RDKTT-4488: Removing RPI box type for WEBM script
    Reason for change: Removing RPI box type for WEBM script
    Test Procedure:Verify with TDK TM
    Risks: None
    Priority : Medium
    Signed-off-by: Pooranik <Poorani_k@comcast.com>
    
    (cherry picked from commit 079edfddff393f013abe004c481a245926b7e2c1)
    Change-Id: I81451ec81d8899f016c98b8e3b65a7e2430657d1

--- framework/web-app/fileStore/testscriptsRDKV/integration/firebolt_native_apps_compliance/FNCS_Playback_Seek_Towards_EOS_with_Pause_Webm.py
+++ framework/web-app/fileStore/testscriptsRDKV/integration/firebolt_native_apps_compliance/FNCS_Playback_Seek_Towards_EOS_with_Pause_Webm.py
@@ -51,10 +51,6 @@
     <box_type>Video_Accelerator</box_type>
     <box_type>RDKTV</box_type>
     <!--  -->
-    <box_type>RPI-CLIENT</box_type>
-    <!--  -->
-    <box_type>RPI-HYB</box_type>
-    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDK2.0</rdk_version>
