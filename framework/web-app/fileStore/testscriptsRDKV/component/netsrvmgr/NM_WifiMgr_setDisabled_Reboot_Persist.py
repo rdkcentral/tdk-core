563879c28c9f923070b3811366969a40816e75db
abhishek.m1 <abhishek.m1@tataelxsi.co.in>
2024-06-07 14:16:35 +0000

    TDK-8100: Enhance coverage for NetSrvMgr
    Reason for change: Add cancel WPS pairing scenario
    Enable/Disable Wifi Adapter reboot persist
    Test Procedure:Verify with TDK build
    Risks: None
    Priority: P1
    Change-Id: I6a79ac5d0f4bc23491141cbfe6b56e2c03063a1a
    Signed-off-by: Abhishek Murali <abhishek_murali@comcast.com>

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/netsrvmgr/NM_WifiMgr_setDisabled_Reboot_Persist.py
@@ -0,0 +1,195 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2024 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>NM_WifiMgr_setDisabled_Reboot_Persist</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>NetSrvMgr_WifiMgr_SetEnabled</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To disable wifi adapter and verify after reboot</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>4</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>CT_NM_33</test_case_id>
+    <test_objective>To disable wifi adapter and verify after reboot</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Video_Accelerator</test_setup>
+    <pre_requisite>1. netSrvMgr should be up and running.
+2. IARMDaemonMain should be up and running.
+3. Connect via ethernet and disconnect wifi</pre_requisite>
+    <api_or_interface_used>IARM_Bus_Init (test agent process_name)
+IARM_Bus_Connect()
+IARM_Bus_Call(IARM_BUS_GetCurrentState)
+IARM_Bus_Disconnect()
+IARM_Bus_Term</api_or_interface_used>
+    <input_parameters>enable</input_parameters>
+    <automation_approch>1. TM loads the NetSrvMgr_Agent via the test agent.
+2.NetSrvMgr_Agent should disable WiFi adapter
+3.GetCurrent state API should return Wifi is disconnected or not
+4.Reboot the device and check the device is still disconnected to WIFI or not</automation_approch>
+    <expected_output>WiFi should disconnected after reboot</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>libnetsrvmgrstub.so</test_stub_interface>
+    <test_script>NM_WifiMgr_setDisabled_Reboot_Persist</test_script>
+    <skipped>No</skipped>
+    <release_version>M125</release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+from iarmbus import IARMBUS_Init,IARMBUS_Connect,IARMBUS_DisConnect,IARMBUS_Term;
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+
+#Test component to be tested
+iarmObj = tdklib.TDKScriptingLibrary("iarmbus","2.0");
+iarmObj.configureTestCase(ip,port,'NM_WifiMgr_setDisabled');
+#Get the result of connection with test component and STB
+iarmLoadStatus = iarmObj.getLoadModuleResult();
+print("Iarmbus module loading status : %s" %iarmLoadStatus) ;
+#Set the module loading status
+iarmObj.setLoadModuleStatus(iarmLoadStatus);
+
+if "SUCCESS" in iarmLoadStatus.upper():
+    #Calling IARMBUS API "IARM_Bus_Init"
+    result = IARMBUS_Init(iarmObj,"SUCCESS")
+    #Check for SUCCESS/FAILURE return value of IARMBUS_Init
+    if "SUCCESS" in result:
+        #Calling IARMBUS API "IARM_Bus_Connect"
+        result = IARMBUS_Connect(iarmObj,"SUCCESS")
+        #Check for SUCCESS/FAILURE return value of IARMBUS_Connect
+        if "SUCCESS" in result:
+            #Test component to be tested
+            netsrvObj = tdklib.TDKScriptingLibrary("netsrvmgr","1");
+            netsrvObj.configureTestCase(ip,port,'NM_WifiMgr_setDisabled');
+            #Get the result of connection with test component and STB
+            netsrvLoadStatus =netsrvObj.getLoadModuleResult();
+            print("[LIB LOAD STATUS]  :  %s" %netsrvLoadStatus);
+            #Set the module loading status
+            netsrvObj.setLoadModuleStatus(netsrvLoadStatus);
+
+            if "SUCCESS" in netsrvLoadStatus.upper():
+                #Prmitive test case which associated to this Script
+                tdkTestObj = netsrvObj.createTestStep('NetSrvMgr_WifiMgr_SetEnabled');
+
+                #Execute the test case in STB
+                tdkTestObj.addParameter("enable", False);
+                expectedresult="SUCCESS"
+                tdkTestObj.executeTestCase(expectedresult);
+                #Get the result of execution
+                actualresult = tdkTestObj.getResult();
+                details = tdkTestObj.getResultDetails();
+                print("SetEnabled Result : %s" %actualresult);
+                print("Details: [%s]"%details);
+
+                if expectedresult in actualresult:
+                    tdkTestObj = netsrvObj.createTestStep('NetSrvMgr_WifiMgr_GetCurrentState');
+
+                    #Execute the test case in STB
+                    expectedresult="SUCCESS"
+                    tdkTestObj.executeTestCase(expectedresult);
+
+                    #Get the result of execution
+                    actualresult = tdkTestObj.getResult();
+                    details = tdkTestObj.getResultDetails();
+                    print(("Get Current State : %s" %actualresult));
+                    print(("Details: [%s]"%details));
+
+                    if expectedresult in actualresult and "Wifi disconnected" in details:
+                        netsrvObj.initiateReboot();
+                        result = IARMBUS_Init(iarmObj,"SUCCESS")
+                        result = IARMBUS_Connect(iarmObj,"SUCCESS")
+
+                        #Execute the test case in STB
+                        expectedresult="SUCCESS"
+                        tdkTestObj.executeTestCase(expectedresult);
+
+                        #Get the result of execution
+                        actualresult = tdkTestObj.getResult();
+                        details = tdkTestObj.getResultDetails();
+                        print(("Get Current State : %s" %actualresult));
+                        print(("Details: [%s]"%details))
+                        
+                        if expectedresult in actualresult and "Wifi disconnected" in details:
+                            tdkTestObj.setResultStatus("SUCCESS");
+                            print("ACTUAL RESULT : Wifi Disconnected reboot was success")
+                            print("[TEST EXECUTION RESULT] : SUCCESS")
+                        else:
+                            tdkTestObj.setResultStatus("FAILURE");
+                            print("ACTUAL RESULT : Wifi Disconnected reboot was failed")
+                            print("[TEST EXECUTION RESULT] : FAILURE")
+                    else:
+                        tdkTestObj.setResultStatus("FAILURE");
+                        print("ACTUAL RESULT : Wifi is not disconnected")
+                        print("[TEST EXECUTION RESULT] : FAILURE")
+                else:
+                    tdkTestObj.setResultStatus("FAILURE");
+                    print("ACTUAL RESULT : Unable to Wifi as disconnected")
+                    print("[TEST EXECUTION RESULT] : FAILURE")
+        netsrvObj.unloadModule("netsrvmgr");
+    else :
+        print("Failed to Load netsrvmgr Module ")
+
+    #Calling IARM_Bus_DisConnect API
+    result = IARMBUS_DisConnect(iarmObj,"SUCCESS")
+    #calling IARMBUS API "IARM_Bus_Term"
+    result = IARMBUS_Term(iarmObj,"SUCCESS")
+    
+    #Unload iarmbus module
+    iarmObj.unloadModule("iarmbus");
+else :
+    print("Failed to Load iarmbus Module ")
+
+
