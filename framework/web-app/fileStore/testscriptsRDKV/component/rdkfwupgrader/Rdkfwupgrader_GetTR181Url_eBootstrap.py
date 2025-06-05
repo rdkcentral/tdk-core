cc60f021d57eb3be5b54b6e8211a4b2ca151a60d
anburaj <anburaj_krishnan@comcast.com>
2025-06-05 08:14:03 +0000

    TDK-10164: Test coverage for M137 - RDK FWUpgrader MW module
    Reason for change: Implemented new scripts for RDKFWupgrader module with the additional coverages.
    Test Procedure:Verify with TDK TM
    Risks: None
    Priority: Medium
    Change-Id: I4cd151a2992f0d30203093f254264d45aeea2f5f
    Signed-off-by: anburaj <anburaj_krishnan@comcast.com>

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/rdkfwupgrader/Rdkfwupgrader_GetTR181Url_eBootstrap.py
@@ -0,0 +1,128 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2025 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>Rdkfwupgrader_GetTR181Url_eBootstrap</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>rdkfwupgrader_GetTR181Url</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>2</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To get a URL from tr181 for the query type Bootstrap</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>1</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>RPI-Client</box_type>
+    <!--  -->
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>RDKFWUPGRADE_105</test_case_id>
+    <test_objective>To get a URL from tr181 for the query type Bootstrap</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Video Accelerator,RPI</test_setup>
+    <pre_requisite></pre_requisite>
+    <api_or_interface_used>GetTR181Url</api_or_interface_used>
+    <input_parameters>eURL - the specific URL to query tr181 for.
+    pUrlOut - pointer to a char buffer to store the output string.
+    szBufSize - the size of the character buffer in argument 1.</input_parameters>
+    <automation_approch>1. TM loads the RDK_fwupgradeAgent.
+    2. RDK_fwupgradeAgent will invoke GetTR181Url API with the valid buffer size along with pointer variable to hold the url data.
+    3. TM will verify the output by having a expected output string and cross verify.
+    4. TM will return SUCCESS or FAILURE based on the result from the above step.</automation_approch>
+    <expected_output>API must return the number of characters which copied to the buffer</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>librdkfwupgraderstub.so.0.0.0</test_stub_interface>
+    <test_script>Rdkfwupgrader_GetTR181Url_eBootstrap</test_script>
+    <skipped></skipped>
+    <release_version>M137</release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib;
+from RdkfwupgraderTestVariables import *
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("rdkfwupgrader","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'Rdkfwupgrader_GetTR181Url_eBootstrap');
+
+#Get the result of connection with test component and DUT
+result =obj.getLoadModuleResult();
+print("[LIB LOAD STATUS]  :  %s" %result);
+obj.setLoadModuleStatus(result);
+
+if "SUCCESS" in result.upper():
+    #Primitive test case which associated to this Script
+    tdkTestObj = obj.createTestStep('rdkfwupgrader_GetTR181Url');
+    tdkTestObj.addParameter("buffer_size", 256);
+    tdkTestObj.addParameter("eURL_enum", 2); #with eBootstrap URL type
+    tdkTestObj.executeTestCase("SUCCESS");
+    #Get the result of execution
+    result = tdkTestObj.getResult();
+    details = tdkTestObj.getResultDetails().replace(r'\n', '\n').strip();
+
+    print("[RESULT] : %s" %result);
+    print("[DETAILS] : \n%s" %details);
+
+    if result == "SUCCESS":
+        if ((not details) or ("error" in details)):
+            print("FAILURE : Empty or error response is not expected");
+            tdkTestObj.setResultStatus("FAILURE");
+        else:
+            print("SUCCESS : Successfully returns the URL details");
+            tdkTestObj.setResultStatus("SUCCESS");
+    else:
+        print("FAILURE : GetTR181Url failed");
+        tdkTestObj.setResultStatus("FAILURE");
+
+    obj.unloadModule("rdkfwupgrader");
+else:
+    print ("LOAD MODULE FAILED")
