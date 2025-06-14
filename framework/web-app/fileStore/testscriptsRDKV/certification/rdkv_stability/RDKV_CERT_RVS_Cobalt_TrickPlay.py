bc8bad7bf9029847767093f1e18afc84a81d57ec
Madhumitha K <madhumitha_krishnasamy@comcast.com>
2025-06-14 10:51:01 +0530

    TDK-10073 PVS: Updated the test scripts
    Reason for change: Updated PVS scripts with code changes
    Test Procedure: Verify with TDK TM
    Risks: None
    
    Change-Id: I515b88234f3e65221921afda069e7bedc097be57
    Signed-off-by: Madhumitha K <madhumitha_krishnasamy@comcast.com>

--- framework/web-app/fileStore/testscriptsRDKV/certification/rdkv_stability/RDKV_CERT_RVS_Cobalt_TrickPlay.py
+++ framework/web-app/fileStore/testscriptsRDKV/certification/rdkv_stability/RDKV_CERT_RVS_Cobalt_TrickPlay.py
@@ -135,7 +135,8 @@ if expectedResult in (result.upper() and pre_condition_status):
     for plugin in plugins_list[:]:
         if plugin not in supported_plugins:
             plugins_list.remove(plugin)
-            plugin_status_needed.pop(plugin)curr_plugins_status_dict = get_plugins_status(obj,plugins_list)
+            plugin_status_needed.pop(plugin)
+    curr_plugins_status_dict = get_plugins_status(obj,plugins_list)
     time.sleep(10)
     status = "SUCCESS"
     if any(curr_plugins_status_dict[plugin] == "FAILURE" for plugin in plugins_list):
--- framework/web-app/fileStore/testscriptsRDKV/certification/rdkv_stability/RDKV_CERT_RVS_ResidentApp_LifeCycleManagement.py
+++ framework/web-app/fileStore/testscriptsRDKV/certification/rdkv_stability/RDKV_CERT_RVS_ResidentApp_LifeCycleManagement.py
@@ -27,7 +27,7 @@
   <status>FREE</status>
   <synopsis>The objective of this test is to do life cycle management of ResidentApp for a minimum of 1000 times.</synopsis>
   <groups_id/>
-  <execution_time>5000</execution_time>
+  <execution_time>1500</execution_time>
   <long_duration>false</long_duration>
   <advanced_script>false</advanced_script>
   <remarks/>
