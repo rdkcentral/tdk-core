a1d8f0e59281aad4f4327f5edf28cab651b87a43
ejose939 <Elizabeth_Jose2@comcast.com>
2025-07-02 15:07:14 +0000

    TDK-10317: Update FCA configuration
    Reason for change: FCA port number changed
    Test Procedure: Run scripts via TDK TM
    Risks: None
    Change-Id: Ie20b3feeac804a339cd04b0ecbc9a7e971e41940
    Signed-off-by: elizabeth<elizabeth.j@tataelxsi.co.in>

--- framework/web-app/fileStore/FireCertAppTestVariables.py
+++ framework/web-app/fileStore/FireCertAppTestVariables.py
@@ -1,2 +1,2 @@
-fca_app_url = "https://firecertapp.rdkcentral.com:8443/?systemui=true&standalone=true"
-fca_report_url= "https://firecertapp.rdkcentral.com:8443/reports/"
+fca_app_url = "https://firecertapp.rdkcentral.com:8444/?systemui=true&standalone=true"
+fca_report_url= "https://firecertapp.rdkcentral.com:8444/reports/"
