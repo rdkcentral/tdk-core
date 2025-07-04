e5b90ba60acd15f37aa1c83d467abb60b100657a
Poorani_k <poorani_k@comcast.com>
2025-07-04 08:53:01 +0000

    TDK-10299: WebPA framework devdelopment
    Reason for change: WebPA framework devdelopment
    Test Procedure:Verify with TDK TM
    Risks: None
    Priority: Medium
    Change-Id: Ifd0efcaae0aa5a8ffbcb3e3b4f61f1d3fd12589d
    Signed-off-by: Pooranik <Poorani_k@comcast.com>

---
+++ framework/web-app/fileStore/rdkv_WebPAlib.py
@@ -0,0 +1,378 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2025 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#########################################################################
+
+import json
+import sys
+import re
+from SSHUtility import *
+import SSHUtility
+import requests
+import json
+import time
+import os
+import subprocess
+import inspect
+import configparser
+from time import sleep
+import pexpect
+
+deviceMAC=""
+password=""
+user_name=""
+sshMethod=""
+#---------------------------------------------------------------
+#INITIALIZE THE MODULE
+#---------------------------------------------------------------
+def init_module (libobj, port, deviceInfo):
+    global deviceIP
+    global devicePort
+    global deviceName
+    global deviceMAC
+    global deviceType
+    global libObj
+
+    deviceIP = libobj.ip;
+    devicePort = port
+    deviceName = deviceInfo["devicename"]
+    deviceType = deviceInfo["boxtype"]
+    libObj = libobj
+    try:
+        deviceMAC = deviceInfo["mac"]
+        SSHUtility.deviceMAC = deviceMAC
+        SSHUtility.realpath = libobj.realpath
+        deviceMAC = deviceMAC.replace(":","")
+    except Exception as e:
+        print("\nException Occurred while getting MAC \n")
+        print(e)
+        print("PLEASE UPDATE MAC ADDRESS in DEVICE CONFIGURATION")
+        os.exit()
+
+#----------------------------------------------------------------------
+# GET DEVICE CONFIGURATION FROM THE DEVICE CONFIG FILE
+# Description  : Read the value of device configuration from the <device>.config file
+# Parameters   : basePath - a string to specify the path of the TM
+#                configKey - a string to specify the configuration whose value needs to be retrieved from the <device>.config file
+# Return Value : value of device configuration or error log in case of failure
+#----------------------------------------------------------------------
+def get_device_config_value (basePath, configKey):
+    deviceConfigFile=""
+    configValue = ""
+    output = ""
+    configPath = basePath + "/"   + "fileStore/tdkvRDKServiceConfig"
+    deviceNameConfigFile = configPath + "/" + deviceName + ".config"
+    deviceTypeConfigFile = configPath + "/" + deviceType + ".config"
+    # Check whether device / platform config files required for
+    # executing the test are present
+    if os.path.exists (deviceNameConfigFile) == True:
+        deviceConfigFile = deviceNameConfigFile
+    elif os.path.exists (deviceTypeConfigFile) == True:
+        deviceConfigFile = deviceTypeConfigFile
+    else:
+        output = "FAILURE : No Device config file found : " + deviceNameConfigFile + " or " + deviceTypeConfigFile
+        print(output)
+    try:
+        if (len (deviceConfigFile) != 0) and (len (configKey) != 0):
+            config = configparser.ConfigParser ()
+            config.read (deviceConfigFile)
+            deviceConfig = config.sections ()[0]
+            configValue =  config.get (deviceConfig, configKey)
+            output = configValue
+        else:
+            output = "FAILURE : DeviceConfig file or key cannot be empty"
+            print(output)
+    except Exception as e:
+        output = "FAILURE : Exception Occurred: [" + inspect.stack()[0][3] + "] " + e.message
+        print(output)
+    return output
+
+#---------------------------------------------------------------
+# GET THE REQUIRED CONFIGURATIONS TO SSH INTO THE DUT
+# Description  : To get the required configurations to SSH into the DUT
+# Return Value : Returns 'SUCCESS' on successful execution or "FAILURE" in case of failure
+#---------------------------------------------------------------
+def webpa_obtainCredentials():
+    config_status = "SUCCESS"
+    result = "SUCCESS"
+    print("[INFO] Retrieving Configuration values from config file.......")
+    configKeyList = ["SSH_METHOD","SSH_USERNAME", "SSH_PASSWORD"]
+    configValues = {}
+    global password
+    global user_name
+    global sshMethod
+    #Get each configuration from device config file
+    for configKey in configKeyList:
+        get_device_config_value(libObj.realpath,configKey)
+        configValues[configKey] = get_device_config_value(libObj.realpath,configKey)
+        if "FAILURE" not in configValues[configKey] and configValues[configKey] != "":
+            print("SUCCESS: Successfully retrieved %s configuration from device config file" %(configKey))
+        else:
+            print("FAILURE: Failed to retrieve %s configuration from device config file" %(configKey))
+            if configValues[configKey] == "":
+                print("\n [INFO] Please configure the %s key in the device config file" %(configKey))
+                result = "FAILURE"
+                break
+    if "FAILURE" != result:
+        if "directSSH" == configValues["SSH_METHOD"]:
+            sshMethod = configValues["SSH_METHOD"]
+            user_name = configValues["SSH_USERNAME"]
+            if configValues["SSH_PASSWORD"] == "None":
+                password = ""
+            else:
+                password = configValues["SSH_PASSWORD"]
+        else:
+            print("FAILURE: Currently only supports directSSH ssh method")
+            config_status = "FAILURE"
+    else:
+        config_status = "FAILURE"
+    return config_status
+
+#-------------------------------------------------------------------
+# EXECUTE A COMMAND IN DUT SHELL AND GET THE OUTPUT
+# Description  : Execute a command in DUT through ssh_and_execute() from SSHUtility library and get the output
+# Parameters   : 1. sshMethod -  string to specify the SSH method to be used
+#                2. credentials - a coma ceparated string to specify the parameters for ssh_and_execute() method. Values are retrieved from <device>.config
+#                       a. credentials[0] - string to specify the DUT IP
+#                       b. credentials[1] - string to specify the username to ssh to DUT
+#                       c. credentials[2] - string to specify the password to ssh to DUT
+#                3. command - string to specify the command to be executed in DUT
+# Return Value : console output of the command executed on DUT
+#-------------------------------------------------------------------
+def execute_CmndInDUT (sshMethod, credentials, command):
+    output = ""
+    if sshMethod == "directSSH":
+        credentialsList = credentials.split(',')
+        host_name = credentialsList[0]
+        user_name = credentialsList[1]
+        password = credentialsList[2]
+    else:
+        #TODO
+        print("Secure ssh to CPE")
+        pass
+    try:
+        output = ssh_and_execute (sshMethod, host_name, user_name, password, command)
+    except Exception as e:
+        print("Exception occured during ssh session")
+        print(e)
+    return output
+
+################################################################################################
+#     Parodus Service check
+################################################################################################
+
+def webpa_parodusstatuscheck():
+    parodusstatus="SUCCESS"
+    config_status=webpa_obtainCredentials()
+    if "FAILURE" not in config_status:
+        credentials = deviceIP + ',' + user_name + ',' + password
+        #check parodus status
+        command="systemctl status parodus | grep running"
+        print("Executing Command : %s" %command)
+        #execute in DUT function
+        result=execute_CmndInDUT(sshMethod, credentials, command)
+        result=result.split("\n")
+        print(result)
+        result=str(result[1])
+        if "active (running)" in result:
+            print(result)
+            print("\nSUCCESS : Successfully get the parodus status")
+        else:
+            print(result)
+            print("\nFAILURE : Parodus Process not running")
+            parodusstatus="FAILURE"
+    else:
+        print("\nFAILURE : Failed to get the device credentials")
+        parodusstatus="FAILURE"
+    return parodusstatus
+
+################################################################################################
+#        GET Device config
+################################################################################################
+
+def webpa_deviceconfig_value(basePath, configKey, configKey2):
+    webpa_deviceconfig_value_status="SUCCESS"
+    configValue = get_device_config_value(basePath, configKey)
+    if "FAILURE" not in configValue:
+        if len(configValue) == 0:
+            output = "[INFO] - Please configure the WEBPA server URL in device config file"
+            print(output)
+            webpa_deviceconfig_value_status="FAILURE"
+        else:
+            output = "[INFO] - WEBPA server URL is configured device config file"
+            print(output)
+            webpa_url = configValue
+            print("\nConfigured WebPA url : " + webpa_url)
+            
+            configValue = get_device_config_value(basePath, configKey2)
+            if "FAILURE" not in configValue:
+                if len(configValue) == 0:
+                    output = "[INFO] - Please configure the AUTH_TOKEN in device config file"
+                    print(output)
+                    webpa_deviceconfig_value_status="FAILURE"
+                else:
+                    output = "[INFO] - AUTH_TOKEN is configured device config file"
+                    print(output)
+                    Auth_Key = configValue
+                    print("\n Authorization Token : " + Auth_Key)
+            else:
+                output="FAILURE : Failed to get the Authorizationn token"
+                print(output)
+                webpa_deviceconfig_value_status="FAILURE"
+
+    else:
+        output="FAILURE : Failed to get the WEBPA server URL"
+        print(output)
+        webpa_deviceconfig_value_status="FAILURE"
+    return webpa_deviceconfig_value_status,webpa_url,Auth_Key
+
+
+#---------------------------------------------------------------
+# WEBPA GET
+#---------------------------------------------------------------
+def webpa_get(paramName,WEBPA_URL,AUTH_TOKEN):
+    webpa_getstatus="SUCCESS"
+    global deviceMAC
+    global password
+    global user_name
+    global sshMethod
+    if sshMethod and user_name:
+        config_status = "SUCCESS"
+    else:
+        config_status = "FAILURE"
+    deviceMACstatus = "SUCCESS"
+    if "FAILURE" not in deviceMACstatus:
+        if config_status != "SUCCESS":
+            config_status=webpa_obtainCredentials()
+        if "FAILURE" not in config_status:            
+            credentials = deviceIP + ',' + user_name + ',' + password
+            WEBPA_URL = WEBPA_URL.strip()
+            AUTH_TOKEN = AUTH_TOKEN.strip()
+            command = "curl -X GET '"+WEBPA_URL+"/mac:"+deviceMAC+"/config?names="+paramName+"'"+" -H 'authorization:"+AUTH_TOKEN+"'"
+            print("\nExecuting Command : %s" %command)
+            #execute in DUT function
+            result=execute_CmndInDUT (sshMethod, credentials, command)
+            result=str(result).split("\n")
+            result=str(result[1])
+            print("\nGet Response : ", result)
+            data = json.loads(result)
+
+            data_type_field =0
+
+            value_field = data["parameters"][0]["value"].strip('"')
+
+            print("Value : ", value_field)
+
+            if value_field == "" or value_field == "EMPTY":
+                print("\nFAILURE : Value field is empty\n")
+                webpa_getstatus="FAILURE"
+            else:
+                data_type_field = data["parameters"][0]["dataType"]
+                print("Data Type : ", data_type_field)
+                print("\nSUCCESS : Value feild is not empty\n")
+
+        else:
+            print("\nFAILURE : Failed to get the device credentials")
+            webpa_getstatus="FAILURE"
+    else:
+         webpa_getstatus="FAILURE"
+    
+    return webpa_getstatus,value_field,data_type_field
+
+
+#---------------------------------------------------------------
+# WEBPA SET
+#---------------------------------------------------------------
+def webpa_set(paramName,testValue,WEBPA_URL,AUTH_TOKEN,dataType):
+    global deviceMAC
+    global password
+    global user_name
+    global sshMethod
+    if sshMethod and user_name:
+        config_status = "SUCCESS"
+    else:
+        config_status = "FAILURE"
+    deviceMACstatus = "SUCCESS"
+    webpa_setstatus="SUCCESS"
+
+    if "FAILURE" not in deviceMACstatus:
+        if config_status != "SUCCESS":
+            config_status=webpa_obtainCredentials()
+        if "FAILURE" not in config_status:
+            credentials = deviceIP + ',' + user_name + ',' + password
+            WEBPA_URL = WEBPA_URL.strip()
+            AUTH_TOKEN = AUTH_TOKEN.strip()
+            command = "curl -X PATCH "+WEBPA_URL+"/mac:"+deviceMAC+"/config -d '{"+'"parameters":[{"dataType":' + str(dataType) + ', "name":"'+paramName+'",  "value": "'+testValue+'"}]}'+"' -H 'authorization:"+AUTH_TOKEN+"'"
+            print("Executing Command : %s\n" %command)
+            #execute in DUT function
+            result=execute_CmndInDUT (sshMethod, credentials, command)
+            result=str(result).split("\n")
+            result=str(result[1])
+            print("\nSet Response : ", result)
+        else:
+            print("\nFAILURE : Failed to get the device credentials")
+            webpa_setstatus="FAILURE"
+    else:
+         webpa_setstatus="FAILURE"
+
+    return webpa_setstatus
+
+#---------------------------------------------------------------
+# WEBPA validation
+#---------------------------------------------------------------
+def webpa_validate_set(paramName, testValue, WEBPA_URL, AUTH_TOKEN):
+    validation_status = "FAILURE"
+    print(f"\n[INFO] Validating if '{paramName}' was set to '{testValue}'")
+
+    global deviceMAC
+    global password
+    global user_name
+    global sshMethod
+    if sshMethod and user_name:
+        config_status = "SUCCESS"
+    else:
+        config_status = "FAILURE"
+    deviceMACstatus = "SUCCESS"
+    if "FAILURE" not in deviceMACstatus:
+        if config_status != "SUCCESS":
+            config_status=webpa_obtainCredentials()
+        if "FAILURE" not in config_status:
+            credentials = deviceIP + ',' + user_name + ',' + password
+            WEBPA_URL = WEBPA_URL.strip()
+            AUTH_TOKEN = AUTH_TOKEN.strip()
+            command = f"curl -X GET '{WEBPA_URL}/mac:{deviceMAC}/config?names={paramName}' -H 'authorization:{AUTH_TOKEN}'"
+            print(f"\nExecuting Command: {command}")
+            result = execute_CmndInDUT(sshMethod, credentials, command)
+            result = str(result).split("\n")[1]
+            data = json.loads(result)
+
+            actual_value = data["parameters"][0]["value"].strip('"')
+
+            if actual_value == testValue:
+                print(f"[SUCCESS] Parameter '{paramName}' is correctly set to '{testValue}'")
+                validation_status = "SUCCESS"
+            else:
+                print(f"[FAILURE] Mismatch! Expected: '{testValue}', Got: '{actual_value}'")
+        else:
+            print("[FAILURE] Failed to obtain WebPA credentials")
+    else:
+        print("[FAILURE] Failed to get MAC address")
+
+    return validation_status
+
+
+#----------------------------------------------------------------------------------------------
---
+++ framework/web-app/fileStore/testscriptsRDKV/component/rdkv_WebPA/RDKV_WebPA_Check_Parodus_Service_Status.py
@@ -0,0 +1,109 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2025 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>RDKV_WebPA_Check_Parodus_Service_Status</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>webpa_parodusstatuscheck</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To verify whether the Parodus Service is currently running</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>1</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>RPI-Client</box_type>
+    <!--  -->
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>rdkvWebPA_10</test_case_id>
+    <test_objective>To verify whether the Parodus Service is currently running</test_objective>
+    <test_type></test_type>
+    <test_setup></test_setup>
+    <pre_requisite></pre_requisite>
+    <api_or_interface_used></api_or_interface_used>
+    <input_parameters></input_parameters>
+    <automation_approch></automation_approch>
+    <expected_output></expected_output>
+    <priority></priority>
+    <test_stub_interface></test_stub_interface>
+    <test_script></test_script>
+    <skipped></skipped>
+    <release_version>M138</release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib; 
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("rdkv_webpa","1",standAlone=True);
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'RDKV_WebPA_Check_Parodus_Service_Status');
+
+#Get the result of connection with test component and DUT
+result =obj.getLoadModuleResult();
+print("[LIB LOAD STATUS]  :  %s" %result);
+obj.setLoadModuleStatus(result.upper());
+
+expectedResult = "SUCCESS"
+
+if "SUCCESS" in result.upper():
+    tdkTestObj = obj.createTestStep('webpa_parodusstatuscheck')
+    tdkTestObj.executeTestCase(expectedResult)
+    actualresult = tdkTestObj.getResultDetails()
+    if expectedResult in actualresult.upper():
+        tdkTestObj.setResultStatus("SUCCESS")
+    else:
+        tdkTestObj.setResultStatus("FAILURE")
+else:
+    print("\nFAILURE : Module Loading Status Failure\n")
+#unload module
+obj.unloadModule("rdkv_webpa");
