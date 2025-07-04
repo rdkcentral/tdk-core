ec7494fdfd2c1ccdc9af0ef1e810b352f0a383ea
shaik_rahim@comcast.com <shaik_rahim@comcast.com>
2025-07-04 11:31:44 +0530

    TDK-10309: TDK DB dump creating and testing
    Reason for change: TDK  DB dump creating and testing
    Test Procedure: with TDK TM
    Risks: Low
    Change-Id: I59504fa33f4b978ff258214d74b2b6ff7dcf615e
    Signed-off-by: shaik rahim <shaik_rahim@comcast.com>

--- framework/grails-app/conf/Config.groovy
+++ framework/grails-app/conf/Config.groovy
@@ -180,5 +180,5 @@ grails.mail.default.to=emailConfig?.get('default_to')
 
 // Add the TDK version property
         tdk {
-            version = "TDK-137"
+            version = "TDK-138"
         }
