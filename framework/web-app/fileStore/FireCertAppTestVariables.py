94115894aa52f04b42ee2e1ca42bac35e425d50c
ejose939 <Elizabeth_Jose2@comcast.com>
2025-07-22 18:14:51 +0000

    TDK-10404: Remove FCA URLs
    Reason for change: Remove hardcoded urls
    Test Procedure: NA
    Risks : None
    
    Change-Id: I26f48c4f304dba6f6e90d8b536b62f1a3b7ba5a2
    Signed-off-by: Elizabeth <elizabeth_jose2@comcast.com>

--- framework/web-app/fileStore/FireCertAppTestVariables.py
+++ framework/web-app/fileStore/FireCertAppTestVariables.py
@@ -1,2 +1,4 @@
-fca_app_url = "https://firecertapp.rdkcentral.com:8444/?systemui=true&standalone=true"
-fca_report_url= "https://firecertapp.rdkcentral.com:8444/reports/"
+#Give the url in which the FCA app is hosted
+fca_app_url = ""
+#Give the url to which the FCA reports to be uploaded
+fca_report_url= ""
