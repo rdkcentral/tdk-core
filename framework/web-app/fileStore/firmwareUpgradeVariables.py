9969e609a47abeca5baf2f27291a2bbaf48b4573
ksr863 <Kavya_SR@comcast.com>
2025-07-23 14:58:20 +0000

    TDK-10503 TDK Coverage for Firmware Upgrade
    Reason for change: Scripts and Utilities for identified scenarios in FirmwareUpgrade component
    Test Procedure: Test in RPI via TDK TM
    Risks: None
    
    Change-Id: I0aaba7bed7820d7653d7ac9acd792a2a21b49a1a

---
+++ framework/web-app/fileStore/firmwareUpgradeVariables.py
@@ -0,0 +1,52 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2025 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+#
+
+# This file contains the variables used for firmware upgrade operations.
+# The user can populate the values as per requirement.
+
+#Target Firmware Names for RPI and BPI
+#For other platforms, the same variable naming convention can be followed: FIRMWARE_UPGRADE_<PLATFORM>
+FIRMWARE_UPGRADE_RPI=""
+FIRMWARE_UPGRADE_BPI=""
+
+#Target Firmware location and Protocol
+#Target and Initial Firmware is placed in the http server location mentioned below
+FIRMWARE_LOCATION=""
+Protocol="http"
+
+#XCONF Server URL
+XCONF_URL = "https://xconf.rdkcentral.com"
+
+
+#Username of DUT
+username = "root"
+
+#Standalone TDK-B Package Installation Files and Path to enable TDK-B in RDK-B Builds
+#Please ensure that the TDK-B package installation files are present in the source path mentioned below
+
+#Source path of TDK Package Installation Files in the Host Machine[TM]
+source_path = ""
+#Name of the TDK-B Installation Script
+install_script = ""
+#Name of the TDK-B Package to be deployed
+tdk_package = ""
+
+#Destination of Package Installation Files in DUT
+dest_path = "/rdklogs"
