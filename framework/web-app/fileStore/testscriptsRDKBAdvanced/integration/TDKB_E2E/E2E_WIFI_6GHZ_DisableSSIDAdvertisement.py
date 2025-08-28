81c3e60195bc5acaa05842781c27db1793d82d9f
svanga970 <sharath_vangapalli@comcast.com>
2025-08-28 18:03:55 +0530

    TDK-10647 : Script development for 6ghz identified E2E scenarios
    Reason for change: Added changes related to 6ghz E2E sceripts in utility and Device configuration files
    Test Procedure: Test in BPI
    Risks: None
    
    Change-Id: I20500780fa76e9d83aad63a629385443ff446577
    Signed-off-by: svanga970 <sharath_vangapalli@comcast.com>

---
+++ framework/web-app/fileStore/testscriptsRDKBAdvanced/integration/TDKB_E2E/E2E_WIFI_6GHZ_DisableSSIDAdvertisement.py
@@ -0,0 +1,233 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2025 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>E2E_WIFI_6GHZ_DisableSSIDAdvertisement</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>tdkb_e2e_SetMultipleParams</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>2</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>Verify that Network name broadcast (SSID advertisement) can be disabled by admin in gateway</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>15</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>BPI</box_type>
+    <!--  -->
+    <box_type>Broadband</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_TDKB_E2E_665</test_case_id>
+    <test_objective>Verify that Network name broadcast (SSID advertisement) can be disabled by admin in gateway</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband, BPI</test_setup>
+    <pre_requisite>Ensure the client setup is up with the IP address assigned by the gateway</pre_requisite>
+    <api_or_interface_used>None</api_or_interface_used>
+    <input_parameters>Device.WiFi.SSID.{i}.SSID
+Device.WiFi.AccessPoint.{i}.Security.KeyPassphrase
+Device.WiFi.AccessPoint.{i}.SSIDAdvertisementEnabled</input_parameters>
+    <automation_approch>1. Load tdkb_e2e module
+2. Using tdkb_e2e_Get, get and save SSIDAdvertisementEnabled status
+3. Set SSIDAdvertisementEnabled to False using tdkb_e2e_SetMultipleParams
+4. Try to connect to Wifi client and check if the ssid is listing or not
+5. Revert the values of SSIDAdvertisementEnabled
+6. Unload the module</automation_approch>
+    <expected_output>The SSID name for 6GHZ should not list when SSIDAdvertisementEnabled  is set to false</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>tdkb_e2e</test_stub_interface>
+    <test_script>E2E_WIFI_6GHZ_DisableSSIDAdvertisement</test_script>
+    <skipped></skipped>
+    <release_version>M140</release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib
+import time
+import tdkbE2EUtility
+from tdkbE2EUtility import *
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("tdkb_e2e","1")
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'E2E_WIFI_6GHZ_DisableSSIDAdvertisement')
+
+#Get the result of connection with test component
+loadmodulestatus = obj.getLoadModuleResult()
+print(f"[LIB LOAD STATUS]  :  {loadmodulestatus}")
+
+if "SUCCESS" in loadmodulestatus.upper():
+    obj.setLoadModuleStatus("SUCCESS")
+    expectedresult = "SUCCESS"
+    finalStatus = "FAILURE"
+    step = 1
+
+    #Parse the device configuration file
+    print(f"TEST STEP {step}: Parse the device configuration file")
+    print(f"EXPECTED RESULT {step}: Should parse the device configuration file successfully")
+    status = parseDeviceConfig(obj)
+    if expectedresult in status:
+        obj.setLoadModuleStatus("SUCCESS")
+        print(f"ACTUAL RESULT {step}: Parsed the device configuration file successfully")
+        print("[TEST EXECUTION RESULT] : SUCCESS")
+
+        #Assign the WIFI parameters names to a variable
+        ssidName = f"Device.WiFi.SSID.{tdkbE2EUtility.ssid_6ghz_index}.SSID"
+        keyPassPhrase = f"Device.WiFi.AccessPoint.{tdkbE2EUtility.ssid_6ghz_index}.Security.KeyPassphrase"
+        ssidAdvertStatus = f"Device.WiFi.AccessPoint.{tdkbE2EUtility.ssid_6ghz_index}.SSIDAdvertisementEnabled"
+
+        #Get the value of the wifi parameters that are currently set.
+        step += 1
+        print(f"TEST STEP {step}: Get the current ssid, keypassphrase, ssidAdvertStatus")
+        print(f"EXPECTED RESULT {step}: Should retrieve the current ssid, keypassphrase, and ssidAdvertStatus")
+        paramList = [ssidName, keyPassPhrase, ssidAdvertStatus]
+        tdkTestObj, status, orgValue = getMultipleParameterValues(obj, paramList)
+        if expectedresult in status:
+            tdkTestObj.setResultStatus("SUCCESS")
+            print(f"ACTUAL RESULT {step}: {orgValue}")
+            print("[TEST EXECUTION RESULT] : SUCCESS")
+
+            # Set the ssidAdvertisement status, SSID name and password"
+            setValuesList = [tdkbE2EUtility.ssid_6ghz_name, tdkbE2EUtility.ssid_6ghz_pwd, 'false']
+            print(f"WIFI parameter values that are set: {setValuesList}")
+
+            list1 = [ssidName, tdkbE2EUtility.ssid_6ghz_name, 'string']
+            list2 = [keyPassPhrase, tdkbE2EUtility.ssid_6ghz_pwd, 'string']
+            list3 = [ssidAdvertStatus, 'false', 'bool']
+
+            #Concatenate the lists with the elements separated by pipe
+            setParamList = list1 + list2 + list3
+            setParamList = "|".join(map(str, setParamList))
+
+            step += 1
+            print(f"TEST STEP {step}: Set the ssid, keypassphrase and disable the ssid advertisement status")
+            print(f"EXPECTED RESULT {step}: Should set the advertisement status, ssid and keypassphrase")
+            tdkTestObj, actualresult, details = setMultipleParameterValues(obj, setParamList)
+            if expectedresult in actualresult:
+                tdkTestObj.setResultStatus("SUCCESS")
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : SUCCESS")
+
+                #Retrieve the values after set and compare
+                newParamList = [ssidName, keyPassPhrase, ssidAdvertStatus]
+                step += 1
+                print(f"TEST STEP {step}: Get the current ssid, keypassphrase and ssidAdvertStatus")
+                print(f"EXPECTED RESULT {step}: Should retrieve the current ssid, keypassphrase and ssidAdvertStatus")
+                tdkTestObj, status, newValues = getMultipleParameterValues(obj, newParamList)
+
+                if expectedresult in status and setValuesList == newValues:
+                    tdkTestObj.setResultStatus("SUCCESS")
+                    print(f"ACTUAL RESULT {step}: {newValues}")
+                    print("[TEST EXECUTION RESULT] : SUCCESS")
+
+                    #Wait for the changes to reflect in client device
+                    time.sleep(60)
+
+                    #Check if the SSID name is listed in wifi client
+                    step += 1
+                    print(f"TEST STEP {step}: Check if the SSID name is listed in wifi client")
+                    print(f"EXPECTED RESULT {step}: SSID should not be broadcasted after disabling advertisement")
+                    status = wlanIsSSIDAvailable(tdkbE2EUtility.ssid_6ghz_name)
+                    if expectedresult not in status:
+                        tdkTestObj.setResultStatus("SUCCESS")
+                        print(f"ACTUAL RESULT {step}: Network name {tdkbE2EUtility.ssid_6ghz_name} is not broadcasted on the network")
+                        print("[TEST EXECUTION RESULT] : SUCCESS")
+                        finalStatus = "SUCCESS"
+                    else:
+                        tdkTestObj.setResultStatus("FAILURE")
+                        print(f"ACTUAL RESULT {step}: Network name {tdkbE2EUtility.ssid_6ghz_name} is broadcasted on the network")
+                        print("[TEST EXECUTION RESULT] : FAILURE")
+                else:
+                    tdkTestObj.setResultStatus("FAILURE")
+                    print(f"ACTUAL RESULT {step}: {newValues}")
+                    print("[TEST EXECUTION RESULT] : FAILURE")
+            else:
+                tdkTestObj.setResultStatus("FAILURE")
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : FAILURE")
+
+            #Prepare the list of parameter values to be reverted
+            list1 = [ssidName, orgValue[0], 'string']
+            list2 = [keyPassPhrase, orgValue[1], 'string']
+            list3 = [ssidAdvertStatus, orgValue[2], 'bool']
+
+            #Concatenate the lists with the elements separated by pipe
+            revertParamList = list1 + list2 + list3
+            revertParamList = "|".join(map(str, revertParamList))
+
+            step += 1
+            print(f"TEST STEP {step}: Revert the values to original")
+            print(f"EXPECTED RESULT {step}: Should set the original ssid, keypassphrase and ssidAdvertStatus")
+            tdkTestObj, actualresult, details = setMultipleParameterValues(obj, revertParamList)
+            if expectedresult in actualresult and "SUCCESS" in finalStatus:
+                tdkTestObj.setResultStatus("SUCCESS")
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : SUCCESS")
+            else:
+                tdkTestObj.setResultStatus("FAILURE")
+                details = tdkTestObj.getResultDetails()
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : FAILURE")
+        else:
+            tdkTestObj.setResultStatus("FAILURE")
+            print(f"ACTUAL RESULT {step}: {orgValue}")
+            print("[TEST EXECUTION RESULT] : FAILURE")
+    else:
+        obj.setLoadModuleStatus("FAILURE")
+        print(f"ACTUAL RESULT {step}: Failed to parse the device configuration file")
+        print("[TEST EXECUTION RESULT] : FAILURE")
+
+    #Handle any post execution cleanup required
+    postExecutionCleanup()
+    obj.unloadModule("tdkb_e2e")
+
+else:
+    print("Failed to load tdkb_e2e module")
+    obj.setLoadModuleStatus("FAILURE")
+    print("Module loading failed")
