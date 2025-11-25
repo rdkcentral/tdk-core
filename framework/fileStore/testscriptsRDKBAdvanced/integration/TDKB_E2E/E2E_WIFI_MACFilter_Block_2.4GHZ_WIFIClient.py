a291d73d9135dcae8423b34566ecb4f2fe9f1f19
svanga970 <sharath_vangapalli@comcast.com>
2025-11-25 17:19:15 +0530

    TDK-8820 : E2E script Issuess
    Reason for change: Added an fix for below script issues
    			E2E_WIFI_MACFilter_Allow_2.4GHZ_WIFIClient
    			E2E_WIFI_MACFilter_Allow_5GHZ_WIFIClient
    			E2E_WIFI_MACFilter_Block2.4_Allow5_WIFIClient
    			E2E_WIFI_MACFilter_Block5_Allow2.4_WIFIClient
    			E2E_WIFI_MACFilter_Block_2.4GHZ_WIFIClient
    			E2E_WIFI_MACFilter_Block_5GHZ_WIFIClient
    			E2E_Firewall_Custom_FtpFromLanToWan
    			E2E_Firewall_Low_FtpFromLanToWan
    			E2E_Firewall_Low_FtpFromWlanToWan
    			E2E_Firewall_Medium_FtpFromLanToWan
    			E2E_WIFI_2.4GHZ_ChannelNumber_ManualMode
    			E2E_WIFI_5GHZ_ChannelNumber_ManualMode
    			E2E_WIFI_5GHZ_Verify_WGSSID_FromWlanClient
    
    Test Procedure: Validated on BPI and RPI
    Risks: None
    email : sharath_vangapalli@comcast.com

--- framework/fileStore/testscriptsRDKBAdvanced/integration/TDKB_E2E/E2E_WIFI_MACFilter_Block_2.4GHZ_WIFIClient.py
+++ framework/fileStore/testscriptsRDKBAdvanced/integration/TDKB_E2E/E2E_WIFI_MACFilter_Block_2.4GHZ_WIFIClient.py
@@ -244,6 +244,7 @@ if "SUCCESS" in loadmodulestatus.upper() and "SUCCESS" in loadmodulestatus1.uppe
                                                         newParamList=[deviceName,deviceMAC]
                                                         time.sleep(60);
                                                         tdkTestObj,status,newValues = getMultipleParameterValues(obj,newParamList)
+                                                        newValues[1] = newValues[1].lower()
                                                         if expectedresult in status and setValuesList == newValues:
                                                             tdkTestObj.setResultStatus("SUCCESS");
                                                             print("TEST STEP 10: Get the current value of name and MAC of client device in the filter")
