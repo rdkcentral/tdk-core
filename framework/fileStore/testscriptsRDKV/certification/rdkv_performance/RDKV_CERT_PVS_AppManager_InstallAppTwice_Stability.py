bffe65d36140d2eabfa567d6edf7d9eeeaa830da
Pooranikanoji <pooranikanoji3@gmail.com>
2026-05-08 11:02:57 +0530

    Update RDKV_CERT_PVS_AppManager_InstallAppTwice_Stability.py

--- framework/fileStore/testscriptsRDKV/certification/rdkv_performance/RDKV_CERT_PVS_AppManager_InstallAppTwice_Stability.py
+++ framework/fileStore/testscriptsRDKV/certification/rdkv_performance/RDKV_CERT_PVS_AppManager_InstallAppTwice_Stability.py
@@ -29,7 +29,7 @@ obj = tdklib.TDKScriptingLibrary("rdkv_performance","1",standAlone=True)
 ip = <ipaddress>
 port = <port>
 
-obj.configureTestCase(ip,port,'RDKV_CERT_PVS_AppManager_ResourceUsage_InstallTwice')
+obj.configureTestCase(ip,port,'RDKV_CERT_PVS_AppManager_InstallAppTwice_Stability')
 
 # Pre-requisite reboot
 pre_requisite_reboot(obj,"yes")
