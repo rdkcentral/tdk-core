1feb96f4858f6c3a6d5a54495e3bdf82ce48268a
Abhishek Murali <74609677+Abhishek-0412@users.noreply.github.com>
2026-06-23 15:24:54 +0530

    Update Graphics_Westeros_Validation.py

--- framework/fileStore/testscriptsRDKV/integration/Graphics/Graphics_Westeros_Validation.py
+++ framework/fileStore/testscriptsRDKV/integration/Graphics/Graphics_Westeros_Validation.py
@@ -26,7 +26,7 @@ obj = tdklib.TDKScriptingLibrary("Graphics","1",standAlone=True)
 #This will be replaced with corresponding DUT Ip and port while executing script
 ip = <ipaddress>
 port = <port>
-obj.configureTestCase(ip,port,'FNCS_Westeros_Validation');
+obj.configureTestCase(ip,port,'Graphics_Westeros_Validation');
 
 #Get the result of connection with test component and DUT
 result =obj.getLoadModuleResult();
