9fde39fbcddbf4512860d2f78cf27683bf02d5cd
Midhun2412 <mohanmidhun084@gmail.com>
2026-08-05 11:55:20 +0530

    Renamed RVS scripts by removing the term Verify

--- framework/fileStore/testscriptsRDKV/certification/rdkv_stability/RDKV_CERT_RVS_AppManager_Check_onAppInstalled_onAppUninstalled_Events.py
+++ framework/fileStore/testscriptsRDKV/certification/rdkv_stability/RDKV_CERT_RVS_AppManager_Check_onAppInstalled_onAppUninstalled_Events.py
@@ -34,7 +34,7 @@ obj = tdklib.TDKScriptingLibrary("rdkv_stability","1",standAlone=True)
 #This will be replaced with corresponding DUT Ip and port while executing script
 ip = <ipaddress>
 port = <port>
-obj.configureTestCase(ip,port,'RDKV_CERT_RVS_AppManager_Verify_onAppInstalled_onAppUninstalled_Events');
+obj.configureTestCase(ip,port,'RDKV_CERT_RVS_AppManager_Check_onAppInstalled_onAppUninstalled_Events');
 
 #The device will reboot before starting the stability testing if "pre_req_reboot" is
 #configured as "Yes".
