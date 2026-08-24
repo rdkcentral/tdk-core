e56b3452aa4f2c9ce1d94f748dfecdf3a91e3202
lraj561 <lekshmi_raj@comcast.com>
2026-08-24 08:40:52 +0000

    TDK-12448 Enhancement required for E2E_SANITY scripts
    Reason for change: Enhancement required for E2E_SANITY scripts
    Test Procedure: Run scripts in BPI
    Risks: None
    
    Changes completed by aaronp2020

--- framework/fileStore/testscriptsRDKBAdvanced/integration/TDKB_E2E/E2E_SANITY_WIFI_CheckSSIDBroadcast.py
+++ framework/fileStore/testscriptsRDKBAdvanced/integration/TDKB_E2E/E2E_SANITY_WIFI_CheckSSIDBroadcast.py
@@ -90,155 +90,182 @@ Device.WiFi.AccessPoint.2.Security.KeyPassphrase</input_parameters>
 </xml>
 '''
 # use tdklib library,which provides a wrapper for tdk testcase script
-import tdklib;
-import time;
+import tdklib
+import time
 import tdkbE2EUtility
-from tdkbE2EUtility import *;
+from tdkbE2EUtility import *
 
 #Test component to be tested
-obj = tdklib.TDKScriptingLibrary("tdkb_e2e","1");
+obj = tdklib.TDKScriptingLibrary("tdkb_e2e","1")
 
 #IP and Port of box, No need to change,
 #This will be replaced with correspoing Box Ip and port while executing script
 ip = <ipaddress>
 port = <port>
-obj.configureTestCase(ip,port,'E2E_SANITY_WIFI_CheckSSIDBroadcast');
+obj.configureTestCase(ip,port,'E2E_SANITY_WIFI_CheckSSIDBroadcast')
 
 #Get the result of connection with test component
-loadmodulestatus =obj.getLoadModuleResult();
-print("[LIB LOAD STATUS]  :  %s" %loadmodulestatus) ;
+loadmodulestatus =obj.getLoadModuleResult()
+print("[LIB LOAD STATUS]  :  %s" %loadmodulestatus)
 
 if "SUCCESS" in loadmodulestatus.upper():
-    obj.setLoadModuleStatus("SUCCESS");
+    obj.setLoadModuleStatus("SUCCESS")
     expectedresult = "SUCCESS"
     finalStatus = "FAILURE"
+    step = 1
+    status = "FAILURE"
 
     #Parse the device configuration file
-    status = parseDeviceConfig(obj);
+    status = parseDeviceConfig(obj)
     if expectedresult in status:
-        obj.setLoadModuleStatus("SUCCESS");
+        obj.setLoadModuleStatus("SUCCESS")
         print("Parsed the device configuration file successfully")
 
-        #Assign the WIFI parameters names to a variable
-        ssidName_2g = "Device.WiFi.SSID.%s.SSID" %tdkbE2EUtility.ssid_2ghz_index
-        keyPassPhrase_2g = "Device.WiFi.AccessPoint.%s.Security.KeyPassphrase" %tdkbE2EUtility.ssid_2ghz_index
+        if tdkbE2EUtility.mlo_capability == "False":
+            #Assign the WIFI parameters names to a variable
+            ssidName_2g = "Device.WiFi.SSID.%s.SSID" %tdkbE2EUtility.ssid_2ghz_index
+            keyPassPhrase_2g = "Device.WiFi.AccessPoint.%s.Security.KeyPassphrase" %tdkbE2EUtility.ssid_2ghz_index
+            ssidName_5g = "Device.WiFi.SSID.%s.SSID" %tdkbE2EUtility.ssid_5ghz_index
+            keyPassPhrase_5g = "Device.WiFi.AccessPoint.%s.Security.KeyPassphrase" %tdkbE2EUtility.ssid_5ghz_index
 
-        ssidName_5g = "Device.WiFi.SSID.%s.SSID" %tdkbE2EUtility.ssid_5ghz_index
-        keyPassPhrase_5g = "Device.WiFi.AccessPoint.%s.Security.KeyPassphrase" %tdkbE2EUtility.ssid_5ghz_index
+            paramList = [ssidName_2g,keyPassPhrase_2g,ssidName_5g,keyPassPhrase_5g]
+            setValuesList = [tdkbE2EUtility.ssid_2ghz_name,tdkbE2EUtility.ssid_2ghz_pwd,tdkbE2EUtility.ssid_5ghz_name,tdkbE2EUtility.ssid_5ghz_pwd]
+            ssidList = [tdkbE2EUtility.ssid_2ghz_name,tdkbE2EUtility.ssid_5ghz_name]
+
+            setList1 = [ssidName_2g,tdkbE2EUtility.ssid_2ghz_name,'string']
+            setList2 = [keyPassPhrase_2g,tdkbE2EUtility.ssid_2ghz_pwd,'string']
+            setList3 = [ssidName_5g,tdkbE2EUtility.ssid_5ghz_name,'string']
+            setList4 = [keyPassPhrase_5g,tdkbE2EUtility.ssid_5ghz_pwd,'string']
+            setParamList = setList1 + setList2 + setList3 + setList4
+        else:
+            #Assign the MLO WIFI parameters names to a variable
+            mloSsidIndex = tdkbE2EUtility.ssid_2ghz_index
+            mloSsidName = "Device.WiFi.SSID.%s.SSID" %mloSsidIndex
+            mloKeyPassPhrase = "Device.WiFi.AccessPoint.%s.Security.KeyPassphrase" %mloSsidIndex
+
+            paramList = [mloSsidName,mloKeyPassPhrase]
+            setValuesList = [tdkbE2EUtility.ssid_name,tdkbE2EUtility.ssid_pwd]
+            ssidList = [tdkbE2EUtility.ssid_name]
+
+            setList1 = [mloSsidName,tdkbE2EUtility.ssid_name,'string']
+            setList2 = [mloKeyPassPhrase,tdkbE2EUtility.ssid_pwd,'string']
+            setParamList = setList1 + setList2
 
         #Get the value of the wifi parameters that are currently set.
-        paramList=[ssidName_2g,keyPassPhrase_2g,ssidName_5g,keyPassPhrase_5g]
         tdkTestObj,status,orgValue = getMultipleParameterValues(obj,paramList)
 
         if expectedresult in status:
-            tdkTestObj.setResultStatus("SUCCESS");
-            print("TEST STEP 1: Get the current ssid,keypassphrase of 2.4GHZ and 5GHZ")
-            print("EXPECTED RESULT 1: Should retrieve the current ssid,keypassphrase of 2.4GHZ and 5GHZ")
-            print("ACTUAL RESULT 1: %s" %orgValue);
-            print("[TEST EXECUTION RESULT] : SUCCESS");
+            tdkTestObj.setResultStatus("SUCCESS")
+            print(f"\nTEST STEP {step}: Get the current WiFi SSID and keypassphrase values")
+            print(f"EXPECTED RESULT {step}: Should retrieve the current WiFi SSID and keypassphrase values")
+            print(f"ACTUAL RESULT {step}: {orgValue}")
+            print("[TEST EXECUTION RESULT] : SUCCESS")
 
-            setValuesList = [tdkbE2EUtility.ssid_2ghz_name,tdkbE2EUtility.ssid_2ghz_pwd,tdkbE2EUtility.ssid_5ghz_name,tdkbE2EUtility.ssid_5ghz_pwd];
             print("WIFI parameter values that are set: %s" %setValuesList)
 
-            list1 = [ssidName_2g,tdkbE2EUtility.ssid_2ghz_name,'string']
-            list2 = [keyPassPhrase_2g,tdkbE2EUtility.ssid_2ghz_pwd,'string']
-            list3 = [ssidName_5g,tdkbE2EUtility.ssid_5ghz_name,'string']
-            list4 = [keyPassPhrase_5g,tdkbE2EUtility.ssid_5ghz_pwd,'string']
-
             #Concatenate the lists with the elements separated by pipe
-            setParamList = list1 + list2 + list3 + list4
             setParamList = "|".join(map(str, setParamList))
 
             tdkTestObj,actualresult,details = setMultipleParameterValues(obj,setParamList)
+            step = step + 1
+
             if expectedresult in actualresult:
-                tdkTestObj.setResultStatus("SUCCESS");
-                print("TEST STEP 2: Set the ssid,keypassphrase of 2.4GHZ and 5GHZ ")
-                print("EXPECTED RESULT 2: Should set the ssid,keypassphrase of 2.4GHZ and 5GHZ");
-                print("ACTUAL RESULT 2: %s" %details);
-                print("[TEST EXECUTION RESULT] : SUCCESS");
+                tdkTestObj.setResultStatus("SUCCESS")
+                print(f"\nTEST STEP {step}: Set the WiFi SSID and keypassphrase values")
+                print(f"EXPECTED RESULT {step}: Should set the WiFi SSID and keypassphrase values")
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : SUCCESS")
+
                 #Retrieve the values after set and compare
-                newParamList=[ssidName_2g,keyPassPhrase_2g,ssidName_5g,keyPassPhrase_5g]
-                tdkTestObj,status,newValues = getMultipleParameterValues(obj,newParamList)
+                tdkTestObj,status,newValues = getMultipleParameterValues(obj,paramList)
+                step = step + 1
 
                 if expectedresult in status and setValuesList == newValues:
-                    tdkTestObj.setResultStatus("SUCCESS");
-                    print("TEST STEP 3: Get the current ssid,keypassphrase of 2.4GHZ and 5GHZ")
-                    print("EXPECTED RESULT 3: Should retrieve the current ssid,keypassphrase of 2.4GHZ and 5GHZ")
-                    print("ACTUAL RESULT 3: %s" %newValues);
-                    print("[TEST EXECUTION RESULT] : SUCCESS");
+                    tdkTestObj.setResultStatus("SUCCESS")
+                    print(f"\nTEST STEP {step}: Get the updated WiFi SSID and keypassphrase values")
+                    print(f"EXPECTED RESULT {step}: Should retrieve the updated WiFi SSID and keypassphrase values")
+                    print(f"ACTUAL RESULT {step}: {newValues}")
+                    print("[TEST EXECUTION RESULT] : SUCCESS")
 
                     #Wait for the changes to reflect in client device
-                    time.sleep(60);
+                    time.sleep(60)
 
                     #Check if the SSID name is listed in wifi client
-                    print("TEST STEP 4: Check if the SSID name is listed in wifi client")
-                    status = wlanIsSSIDAvailable(tdkbE2EUtility.ssid_2ghz_name);
-                    if expectedresult in status:
-                        tdkTestObj.setResultStatus("SUCCESS");
-                        print("Network name",tdkbE2EUtility.ssid_2ghz_name,"is broadcasted on the network");
-                        status = wlanIsSSIDAvailable(tdkbE2EUtility.ssid_5ghz_name);
-                        if expectedresult in status:
-                            tdkTestObj.setResultStatus("SUCCESS");
-                            print("Network name",tdkbE2EUtility.ssid_5ghz_name,"is broadcasted on the network");
-                            finalStatus = "SUCCESS"
-                        else:
-                            tdkTestObj.setResultStatus("SUCCESS");
-                            print("Network name",tdkbE2EUtility.ssid_5ghz_name,"is not broadcasted on the network");
-                    else:
-                        tdkTestObj.setResultStatus("FAILURE");
-                        print("Network name",tdkbE2EUtility.ssid_2ghz_name,"is not broadcasted on the network");
+                    step = step + 1
+                    print(f"\nTEST STEP {step}: Check if the configured WiFi SSID names are listed in WiFi client")
+                    print(f"EXPECTED RESULT {step}: Configured WiFi SSID names should be broadcasted on the network")
 
+                    ssidBroadcastStatus = "SUCCESS"
+                    for ssid in ssidList:
+                        status = wlanIsSSIDAvailable(ssid)
+                        if expectedresult in status:
+                            tdkTestObj.setResultStatus("SUCCESS")
+                            print("Network name",ssid,"is broadcasted on the network")
+                        else:
+                            ssidBroadcastStatus = "FAILURE"
+                            tdkTestObj.setResultStatus("FAILURE")
+                            print("Network name",ssid,"is not broadcasted on the network")
+
+                    if expectedresult in ssidBroadcastStatus:
+                        finalStatus = "SUCCESS"
+                        print(f"ACTUAL RESULT {step}: Configured WiFi SSID names are broadcasted")
+                        print("[TEST EXECUTION RESULT] : SUCCESS")
+                    else:
+                        print(f"ACTUAL RESULT {step}: One or more configured WiFi SSID names are not broadcasted")
+                        print("[TEST EXECUTION RESULT] : FAILURE")
                 else:
-                    tdkTestObj.setResultStatus("FAILURE");
-                    print("TEST STEP 3: Get the current ssid,keypassphrase of 2.4GHZ and 5GHZ")
-                    print("EXPECTED RESULT 3: Should retrieve the current ssid,keypassphrase of 2.4GHZ and 5GHZ")
-                    print("ACTUAL RESULT 3: %s" %newValues);
-                    print("[TEST EXECUTION RESULT] : FAILURE");
+                    tdkTestObj.setResultStatus("FAILURE")
+                    print(f"\nTEST STEP {step}: Get the updated WiFi SSID and keypassphrase values")
+                    print(f"EXPECTED RESULT {step}: Should retrieve the updated WiFi SSID and keypassphrase values")
+                    print(f"ACTUAL RESULT {step}: {newValues}")
+                    print("[TEST EXECUTION RESULT] : FAILURE")
             else:
-                tdkTestObj.setResultStatus("FAILURE");
-                print("TEST STEP 2: Set the ssid,keypassphrase of 2.4GHZ and 5GHZ")
-                print("EXPECTED RESULT 2: Should set the ssid,keypassphrase of 2.4GHZ and 5GHZ");
-                print("ACTUAL RESULT 2: %s" %details);
-                print("[TEST EXECUTION RESULT] : FAILURE");
+                tdkTestObj.setResultStatus("FAILURE")
+                print(f"\nTEST STEP {step}: Set the WiFi SSID and keypassphrase values")
+                print(f"EXPECTED RESULT {step}: Should set the WiFi SSID and keypassphrase values")
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : FAILURE")
 
             #Prepare the list of parameter values to be reverted
-            list1 = [ssidName_2g,orgValue[0],'string']
-            list2 = [keyPassPhrase_2g,orgValue[1],'string']
-            list3 = [ssidName_5g,orgValue[2],'string']
-            list4 = [keyPassPhrase_5g,orgValue[3],'string']
+            revertParamList = []
+            index = 0
+            for param in paramList:
+                revertParamList = revertParamList + [param,orgValue[index],'string']
+                index = index + 1
 
             #Concatenate the lists with the elements separated by pipe
-            revertParamList = list1 + list2 + list3 + list4
             revertParamList = "|".join(map(str, revertParamList))
 
             #Revert the values to original
+            step = step + 1
+            print(f"\nTEST STEP {step}: Revert the WiFi SSID and keypassphrase values to original")
+            print(f"EXPECTED RESULT {step}: Should set the original WiFi SSID and keypassphrase values")
             tdkTestObj,actualresult,details = setMultipleParameterValues(obj,revertParamList)
+
             if expectedresult in actualresult and expectedresult in finalStatus:
-                tdkTestObj.setResultStatus("SUCCESS");
-                print("EXPECTED RESULT 15: Should set the original ssid,keypassphrase of 2.4GHZ and 5GHZ");
-                print("ACTUAL RESULT 15: %s" %details);
-                print("[TEST EXECUTION RESULT] : SUCCESS");
+                tdkTestObj.setResultStatus("SUCCESS")
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : SUCCESS")
             else:
-                tdkTestObj.setResultStatus("FAILURE");
-                details = tdkTestObj.getResultDetails();
-                print("EXPECTED RESULT 15: Should set the original ssid,keypassphrase of 2.4GHZ and 5GHZ");
-                print("ACTUAL RESULT 15: %s" %details);
-                print("[TEST EXECUTION RESULT] : FAILURE");
+                tdkTestObj.setResultStatus("FAILURE")
+                details = tdkTestObj.getResultDetails()
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : FAILURE")
         else:
-            tdkTestObj.setResultStatus("FAILURE");
-            print("TEST STEP 1: Get the current ssid,keypassphrase of 2.4GHZ and 5GHZ")
-            print("EXPECTED RESULT 1: Should retrieve the current ssid,keypassphrase of 2.4GHZ and 5GHZ")
-            print("ACTUAL RESULT 1: %s" %orgValue);
-            print("[TEST EXECUTION RESULT] : FAILURE");
+            tdkTestObj.setResultStatus("FAILURE")
+            print(f"\nTEST STEP {step}: Get the current WiFi SSID and keypassphrase values")
+            print(f"EXPECTED RESULT {step}: Should retrieve the current WiFi SSID and keypassphrase values")
+            print(f"ACTUAL RESULT {step}: {orgValue}")
+            print("[TEST EXECUTION RESULT] : FAILURE")
     else:
-        obj.setLoadModuleStatus("FAILURE");
+        obj.setLoadModuleStatus("FAILURE")
         print("Failed to parse the device configuration file")
 
     #Handle any post execution cleanup required
-    postExecutionCleanup();
-    obj.unloadModule("tdkb_e2e");
+    postExecutionCleanup()
+    obj.unloadModule("tdkb_e2e")
 
 else:
-    print("Failed to load tdkb_e2e module");
-    obj.setLoadModuleStatus("FAILURE");
-    print("Module loading failed");
+    print("Failed to load tdkb_e2e module")
+    obj.setLoadModuleStatus("FAILURE")
+    print("Module loading failed")
