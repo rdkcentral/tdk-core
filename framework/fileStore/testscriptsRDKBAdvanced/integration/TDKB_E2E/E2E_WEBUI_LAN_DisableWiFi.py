0c0ea02cb976c43dbed91d01f32d002f307e8490
lraj561 <lekshmi_raj@comcast.com>
2026-08-24 08:29:23 +0000

    TDK-12447 E2E WEBUI Script Development
    Reason for change: E2E WEBUI Script Development
    Test Procedure: Run scripts in BPI
    Risks: None
    
    Changes completed by aaronp2020

---
+++ framework/fileStore/testscriptsRDKBAdvanced/integration/TDKB_E2E/E2E_WEBUI_LAN_DisableWiFi.py
@@ -0,0 +1,213 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2026 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib
+import time
+import tdkbE2EUtility
+from tdkbE2EUtility import *
+import tdkbWEBUIUtility
+from tdkbWEBUIUtility import *
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("tdkb_e2e","1")
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'E2E_WEBUI_LAN_DisableWiFi')
+
+#Get the result of connection with test component
+loadmodulestatus =obj.getLoadModuleResult()
+print(f"[LIB LOAD STATUS]  :  {loadmodulestatus}")
+
+if "SUCCESS" in loadmodulestatus.upper():
+    obj.setLoadModuleStatus("SUCCESS")
+    expectedresult = "SUCCESS"
+    finalStatus = "FAILURE"
+    step = 1
+
+    #Parse the device configuration file
+    status = parseDeviceConfig(obj)
+    if expectedresult in status:
+        obj.setLoadModuleStatus("SUCCESS")
+        print("Parsed the device configuration file successfully")
+
+        # Check if the device is MLO capable and get the SSID name accordingly
+        if tdkbE2EUtility.mlo_capability == "True":
+            ssidName = tdkbE2EUtility.ssid_name
+        else:
+            ssidName = tdkbE2EUtility.ssid_2ghz_name
+
+        wifiEnable = "Device.WiFi.SSID.%s.Enable" %tdkbE2EUtility.ssid_2ghz_index
+        #Get the value of the wifi parameters that are currently set.
+        paramList=[wifiEnable]
+        tdkTestObj,status,orgValue = getMultipleParameterValues(obj,paramList)
+
+        if expectedresult in status:
+            tdkTestObj.setResultStatus("SUCCESS")
+            print(f"TEST STEP {step}: Get the current wifienable status")
+            print(f"EXPECTED RESULT {step}: Should retrieve the current wifienable status")
+            print(f"ACTUAL RESULT {step}: {orgValue}")
+            print("[TEST EXECUTION RESULT] : SUCCESS")
+
+            if orgValue[0] == "false":
+                step += 1
+                setValuesList = ['true']
+                print(f"WIFI parameter values that are set: {setValuesList}")
+                setParamList = "%s|true|bool" %(wifiEnable)
+                tdkTestObj,actualresult,details = setMultipleParameterValues(obj,setParamList)
+                if expectedresult in actualresult:
+                    tdkTestObj.setResultStatus("SUCCESS")
+                    print(f"TEST STEP {step}: Set the wifi enable status to true")
+                    print(f"EXPECTED RESULT {step}: Should set the wifi enable status to true")
+                    print(f"ACTUAL RESULT {step}: {details}")
+                    print("[TEST EXECUTION RESULT] : SUCCESS")
+                    #Retrieve the values after set and compare
+                    newParamList=[wifiEnable]
+                    tdkTestObj,status,newValues = getMultipleParameterValues(obj,newParamList)
+
+                    step += 1
+                    if expectedresult in status and setValuesList == newValues:
+                        tdkTestObj.setResultStatus("SUCCESS")
+                        print(f"TEST STEP {step}: Get the current wifi enable status")
+                        print(f"EXPECTED RESULT {step}: Should retrieve the current wifi enable status")
+                        print(f"ACTUAL RESULT {step}: {newValues}")
+                        print("[TEST EXECUTION RESULT] : SUCCESS")
+
+                        #Wait for the changes to reflect in client device
+                        time.sleep(60)
+                    else:
+                        tdkTestObj.setResultStatus("FAILURE")
+                        print(f"TEST STEP {step}: Failed to set the wifi enable status to true")
+                        print(f"EXPECTED RESULT {step}: Should retrieve the wifi enable status as true")
+                        print("[TEST EXECUTION RESULT] : FAILURE")
+                        obj.unloadModule("tdkb_e2e")
+                        exit()
+
+            #Connect to LAN client and obtain its IP
+            step += 1
+            print(f"TEST STEP {step}: Get the IP address of the lan client after connecting to it")
+            lanIP = getLanIPAddress(tdkbE2EUtility.lan_interface)
+            if lanIP:
+                tdkTestObj.setResultStatus("SUCCESS")
+                step += 1
+                print(f"TEST STEP {step}: Get the current LAN IP address DHCP range")
+                param = "Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanIPAddress"
+                tdkTestObj,status,curIPAddress = getParameterValue(obj,param)
+                print(f"LAN IP Address: {curIPAddress}")
+
+                if expectedresult in status and curIPAddress:
+                    tdkTestObj.setResultStatus("SUCCESS")
+                    step += 1
+                    print(f"TEST STEP {step}: Check whether lan ip address is in same DHCP range")
+                    status = "SUCCESS"
+                    status = checkIpRange(curIPAddress,lanIP)
+                    if expectedresult in status:
+                        tdkTestObj.setResultStatus("SUCCESS")
+                        #Set Selenium grid
+                        driver,status = startSeleniumGrid(tdkTestObj,"LAN",tdkbE2EUtility.grid_url)
+                        if status == "SUCCESS":
+                            try:
+                                time.sleep(10)
+                                #To click on option "Connection" in UI
+                                driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/ul/li[1]/ul/li[2]/a').click()
+                                #To click on WiFi option under connection
+                                driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/ul/li[1]/ul/li[2]/ul/li[4]/a").click()
+                                time.sleep(10)
+                                # To click on edit option for 2.4GHz wifi
+                                driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[3]/div[2]/table/tbody/tr[2]/td[5]/a").click()
+                                # To Disable 2.4Ghz WiFi
+                                driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[3]/div[2]/form/div[1]/span[2]/ul/a[2]/li/label").click()
+                                #To save the settings
+                                driver.find_element_by_id("save_settings").submit()
+                                time.sleep(60)
+                                driver.quit()
+
+                                #Check if 2.4GHz wifi broadcasting is stopped to validate the disabling of wifi
+                                step += 1
+                                print(f"TEST STEP {step}: Check if the SSID name is listed in wifi client")
+                                time.sleep(30)
+                                status = wlanIsSSIDAvailable(ssidName)
+                                if expectedresult not in status:
+                                    tdkTestObj.setResultStatus("SUCCESS")
+                                    print(f"Network name {ssidName} is not broadcasted on the network")
+                                else:
+                                    tdkTestObj.setResultStatus("FAILURE")
+                                    print(f"Network name {ssidName} is broadcasted on the network")
+                            except Exception as error:
+                                tdkTestObj.setResultStatus("FAILURE")
+                                print(error)
+                        else:
+                            tdkTestObj.setResultStatus("FAILURE")
+                            print("Failed to set selenium grid")
+                        #Kill selenium hub and node
+                        status = tdkbWEBUIUtility.kill_hub_node("LAN")
+                        if status == "SUCCESS":
+                            tdkTestObj.setResultStatus("SUCCESS")
+                            print("Post-requisite success")
+                        else:
+                            tdkTestObj.setResultStatus("FAILURE")
+                            print("Couldnt kill node and hub")
+                    else:
+                        tdkTestObj.setResultStatus("FAILURE")
+                        print("checkIpRange:lan ip address is not in DHCP range")
+                else:
+                    tdkTestObj.setResultStatus("FAILURE")
+                    print("getParameterValue : Failed to get gateway lan ip")
+            else:
+                tdkTestObj.setResultStatus("FAILURE")
+                print("getLanIPAddress:Failed to get the LAN client IP")
+
+            #Prepare the list of parameter values to be reverted
+            revertParamList = "%s|%s|bool" %(wifiEnable,orgValue[0])
+
+            #Revert the values to original
+            tdkTestObj,actualresult,details = setMultipleParameterValues(obj,revertParamList)
+            step += 1
+            if expectedresult in actualresult:
+                tdkTestObj.setResultStatus("SUCCESS")
+                print(f"EXPECTED RESULT {step}: Should set the original wifi enable status")
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : SUCCESS")
+            else:
+                tdkTestObj.setResultStatus("FAILURE")
+                details = tdkTestObj.getResultDetails()
+                print(f"EXPECTED RESULT {step}: Should set the original wifi enable status")
+                print(f"ACTUAL RESULT {step}: {details}")
+                print("[TEST EXECUTION RESULT] : FAILURE")
+        else:
+            tdkTestObj.setResultStatus("FAILURE")
+            print(f"TEST STEP {step}: Get the current wifi enable status")
+            print(f"EXPECTED RESULT {step}: Should retrieve the current wifi enable status")
+            print(f"ACTUAL RESULT {step}: {orgValue}")
+            print("[TEST EXECUTION RESULT] : FAILURE")
+    else:
+        obj.setLoadModuleStatus("FAILURE")
+        print("Failed to parse the device configuration file")
+
+    #Handle any post execution cleanup required
+    postExecutionCleanup()
+    obj.unloadModule("tdkb_e2e")
+
+else:
+    print("Failed to load tdkb_e2e module")
+    obj.setLoadModuleStatus("FAILURE")
+    print("Module loading failed")
---
+++ framework/fileStore/testscriptsRDKBAdvanced/integration/TDKB_E2E/E2E_WEBUI_WLAN_GetNoOfClientsConnected.py
@@ -0,0 +1,251 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2026 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib
+import time
+import tdkbE2EUtility
+from tdkbE2EUtility import *
+import tdkbWEBUIUtility
+from tdkbWEBUIUtility import *
+
+# Test component to be tested
+obj = tdklib.TDKScriptingLibrary("tdkb_e2e","1")
+
+# IP and Port of box, No need to change,
+# This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'E2E_WEBUI_WLAN_GetNoOfClientsConnected')
+
+# Get the result of connection with test component and STB
+loadmodulestatus = obj.getLoadModuleResult()
+print(f"[LIB LOAD STATUS]  :  {loadmodulestatus}")
+
+if "SUCCESS" in loadmodulestatus.upper():
+    obj.setLoadModuleStatus("SUCCESS")
+    expectedresult = "SUCCESS"
+    finalStatus = "FAILURE"
+    step = 1
+    # Parse the device configuration file
+    status = parseDeviceConfig(obj)
+    if expectedresult in status:
+        obj.setLoadModuleStatus("SUCCESS")
+        print("Parsed the device configuration file successfully")
+
+        # Assign the WIFI parameters names to a variable
+        ssidName = "Device.WiFi.SSID.%s.SSID" % tdkbE2EUtility.ssid_2ghz_index
+        keyPassPhrase = "Device.WiFi.AccessPoint.%s.Security.KeyPassphrase" % tdkbE2EUtility.ssid_2ghz_index
+        noOfClientsConnected = "Device.WiFi.AccessPoint.%s.AssociatedDeviceNumberOfEntries" % tdkbE2EUtility.ssid_2ghz_index
+
+        # Get the value of the wifi parameters that are currently set.
+        paramList = [ssidName, keyPassPhrase, noOfClientsConnected]
+        tdkTestObj, status, orgValue = getMultipleParameterValues(obj, paramList)
+
+        if expectedresult in status:
+            tdkTestObj.setResultStatus("SUCCESS")
+            print(f"TEST STEP {step}: Get the current ssid and keypassphrase and number of clients connected to WLAN 2.4GHZ")
+            print(f"EXPECTED RESULT {step}: Should retrieve the current ssid and keypassphrase and  clients connected count")
+            print(f"ACTUAL RESULT {step}: {orgValue}")
+            print("[TEST EXECUTION RESULT] : SUCCESS")
+
+            # Check whether MLO is enabled. If enabled, proceed with MLO SSID and Passphrase. If disabled, set 2.4 GHz SSID and password
+            if tdkbE2EUtility.mlo_capability == "False":
+                print("MLO is disabled in the device configuration file.")
+
+                step += 1
+
+                tdkbE2EUtility.ssid_name = tdkbE2EUtility.ssid_2ghz_name
+                tdkbE2EUtility.ssid_pwd = tdkbE2EUtility.ssid_2ghz_pwd
+                tdkbE2EUtility.wlan_interface = tdkbE2EUtility.wlan_2ghz_interface
+
+                setValuesList = [tdkbE2EUtility.ssid_name,tdkbE2EUtility.ssid_pwd]
+                print(f"WIFI parameter values that are set: {setValuesList}")
+
+                list1 = [ssidName, tdkbE2EUtility.ssid_name, 'string']
+                list2 = [keyPassPhrase, tdkbE2EUtility.ssid_pwd, 'string']
+
+                # Concatenate the lists with the elements separated by pipe
+                setParamList = list1 + list2
+                setParamList = "|".join(map(str, setParamList))
+
+                tdkTestObj, actualresult, details = setMultipleParameterValues(obj, setParamList)
+                if expectedresult in actualresult:
+                    tdkTestObj.setResultStatus("SUCCESS")
+                    print(f"TEST STEP {step}: Set the ssid and keypassphrase")
+                    print(f"EXPECTED RESULT {step}: Should set the ssid and keypassphrase")
+                    print(f"ACTUAL RESULT {step}: {details}")
+                    print("[TEST EXECUTION RESULT] : SUCCESS")
+
+                    # Retrieve the values after set and compare
+                    newParamList = [ssidName, keyPassPhrase]
+                    tdkTestObj, status, newValues = getMultipleParameterValues(obj, newParamList)
+
+                    step += 1
+                    if expectedresult in status and setValuesList == newValues:
+                        tdkTestObj.setResultStatus("SUCCESS")
+                        print(f"TEST STEP {step}: Get the current ssid and keypassphrase")
+                        print(f"EXPECTED RESULT {step}: Should retrieve the current ssid and keypassphrase")
+                        print(f"ACTUAL RESULT {step}: {newValues}")
+                        print("[TEST EXECUTION RESULT] : SUCCESS")
+                    else:
+                        tdkTestObj.setResultStatus("FAILURE")
+                        print(f"TEST STEP {step}: Get the current ssid and keypassphrase")
+                        print(f"EXPECTED RESULT {step}: Should retrieve the current ssid and keypassphrase")
+                        print(f"ACTUAL RESULT {step}: {newValues}")
+                        print("[TEST EXECUTION RESULT] : FAILURE")
+                        status = "FAILURE"
+                else:
+                    tdkTestObj.setResultStatus("FAILURE")
+                    print(f"TEST STEP {step}: Set the ssid and keypassphrase")
+                    print(f"EXPECTED RESULT {step}: Should set the ssid and keypassphrase")
+                    print(f"ACTUAL RESULT {step}: {details}")
+                    print("[TEST EXECUTION RESULT] : FAILURE")
+                    status = "FAILURE"
+            else:
+                print("MLO is enabled in the device configuration file.")
+
+            if status == "SUCCESS":
+                # Wait for the changes to reflect in client device
+                time.sleep(60)
+
+                # Set Selenium grid
+                driver, status = startSeleniumGrid(tdkTestObj, "WLAN", tdkbE2EUtility.grid_url)
+                if status == "SUCCESS":
+                    # Connect to the wifi ssid from wlan client
+                    step += 1
+                    print(f"TEST STEP {step}: From wlan client, Connect to the wifi ssid")
+                    status = wlanConnectWifiSsid(tdkbE2EUtility.ssid_name, tdkbE2EUtility.ssid_pwd, tdkbE2EUtility.wlan_interface)
+                    if expectedresult in status:
+                        tdkTestObj.setResultStatus("SUCCESS")
+
+                        step += 1
+                        print(f"TEST STEP {step}: Get the IP address of the wlan client after connecting to wifi")
+                        wlanIP = getWlanIPAddress(tdkbE2EUtility.wlan_interface)
+                        if wlanIP:
+                            tdkTestObj.setResultStatus("SUCCESS")
+
+                            step += 1
+                            print(f"TEST STEP {step}: Get the current LAN IP address DHCP range")
+                            param = "Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanIPAddress"
+                            tdkTestObj, status, curIPAddress = getParameterValue(obj, param)
+                            print(f"LAN IP Address: {curIPAddress}")
+
+                            if expectedresult in status and curIPAddress:
+                                tdkTestObj.setResultStatus("SUCCESS")
+
+                                step += 1
+                                print(f"TEST STEP {step}: Check whether wlan ip address is in same DHCP range")
+                                status = "SUCCESS"
+                                status = checkIpRange(curIPAddress, wlanIP)
+                                if expectedresult in status:
+                                    tdkTestObj.setResultStatus("SUCCESS")
+                                    status, driver = openLocalWebUI(tdkbE2EUtility.grid_url, tdkTestObj, "LocalLogin")
+                                    if status == "SUCCESS":
+                                        try:
+                                            time.sleep(5)
+                                            driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/ul/li[1]/ul/li[2]/a").click()
+                                            driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/ul/li[1]/ul/li[2]/ul/li[1]/a").click()
+                                            num = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[3]/div[3]/div[1]/div[4]/span[2]").text
+                                            print(f"number of clients connected to WiFi network :{num}")
+
+                                            if int(num) == int(orgValue[2])+1:
+                                                tdkTestObj.setResultStatus("SUCCESS")
+                                                print("Number of clients connected to WiFi network is successfully updated")
+                                                finalStatus = "SUCCESS"
+                                            else:
+                                                tdkTestObj.setResultStatus("FAILURE")
+                                                print("Failed to update the number of clients connected")
+                                        except Exception as error:
+                                            tdkTestObj.setResultStatus("FAILURE")
+                                            print(error)
+                                        time.sleep(10)
+                                        driver.quit()
+                                    else:
+                                        tdkTestObj.setResultStatus("FAILURE")
+                                        print("Failed to open WebUI")
+                                else:
+                                    tdkTestObj.setResultStatus("FAILURE")
+                                    print("checkIpRange:wlan ip address is not in DHCP range")
+                            else:
+                                tdkTestObj.setResultStatus("FAILURE")
+                                print("getParameterValue : Failed to get gateway lan ip")
+                        else:
+                            tdkTestObj.setResultStatus("FAILURE")
+                            print("getWlanIPAddress:Failed to get the wlan ip address")
+                    else:
+                        tdkTestObj.setResultStatus("FAILURE")
+                        print("wlanConnectWifiSsid: Failed to connect to the wifi ssid")
+                else:
+                    tdkTestObj.setResultStatus("FAILURE")
+                    print("Failed to set the selenium grid")
+
+                # Kill selenium hub and node
+                status = tdkbWEBUIUtility.kill_hub_node("LAN")
+                if status == "SUCCESS":
+                    tdkTestObj.setResultStatus("SUCCESS")
+                    print("Post-requisite success")
+                else:
+                    tdkTestObj.setResultStatus("FAILURE")
+                    print("Couldnt kill node and hub")
+
+            if tdkbE2EUtility.mlo_capability == "False":
+                print("Reverting the ssid and keypassphrase to original values")
+                # Prepare the list of parameter values to be reverted
+                list1 = [ssidName, orgValue[0], 'string']
+                list2 = [keyPassPhrase, orgValue[1], 'string']
+
+                # Concatenate the lists with the elements separated by pipe
+                revertParamList = list1 + list2
+                revertParamList = "|".join(map(str, revertParamList))
+
+                # Revert the values to original
+                tdkTestObj, actualresult, details = setMultipleParameterValues(obj, revertParamList)
+                step += 1
+                if expectedresult in actualresult and expectedresult in finalStatus:
+                    tdkTestObj.setResultStatus("SUCCESS")
+                    print(f"EXPECTED RESULT {step}: Should set the original ssid and keypassphrase")
+                    print(f"ACTUAL RESULT {step}: {details}")
+                    print("[TEST EXECUTION RESULT] : SUCCESS")
+                else:
+                    tdkTestObj.setResultStatus("FAILURE")
+                    details = tdkTestObj.getResultDetails()
+                    print(f"EXPECTED RESULT {step}: Should set the original ssid and keypassphrase")
+                    print(f"ACTUAL RESULT {step}: {details}")
+                    print("[TEST EXECUTION RESULT] : FAILURE")
+            else:
+                print("MLO is enabled; no SSID/keypassphrase revert is required.")
+        else:
+            tdkTestObj.setResultStatus("FAILURE")
+            print(f"TEST STEP {step}: Get the current ssid and keypassphrase and clients connected count")
+            print(f"EXPECTED RESULT {step}: Should retrieve the current ssid and keypassphrase and clients connected count")
+            print(f"ACTUAL RESULT {step}: {orgValue}")
+            print("[TEST EXECUTION RESULT] : FAILURE")
+
+    else:
+        obj.setLoadModuleStatus("FAILURE")
+        print("Failed to parse the device configuration file")
+
+    # Handle any post execution cleanup required
+    postExecutionCleanup()
+    obj.unloadModule("tdkb_e2e")
+
+else:
+    print("Failed to load tdkb_e2e module")
+    obj.setLoadModuleStatus("FAILURE")
+    print("Module loading failed")
