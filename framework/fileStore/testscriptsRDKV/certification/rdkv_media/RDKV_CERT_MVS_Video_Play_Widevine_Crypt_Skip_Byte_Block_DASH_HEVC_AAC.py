6eafb2f7d6ba82ede34369acb350e8c0c65ce900
asures684 <aromal_suresh@comcast.com>
2026-09-07 16:29:13 +0530

    TDK-12336: MVS DRM Script Updates [Sprint M153]
    Reason for change: Update the drm test script naming issues
    Test Procedure: Verify with TDK TM
    Risks: None
    Signed-off-by: Aromal Suresh <aromal_suresh@comcast.com>

--- framework/fileStore/testscriptsRDKV/certification/rdkv_media/RDKV_CERT_MVS_Video_Play_Widevine_Crypt_Skip_Byte_Block_DASH_HEVC_AAC.py
+++ framework/fileStore/testscriptsRDKV/certification/rdkv_media/RDKV_CERT_MVS_Video_Play_Widevine_Crypt_Skip_Byte_Block_DASH_HEVC_AAC.py
@@ -29,7 +29,7 @@ obj = tdklib.TDKScriptingLibrary("rdkv_media","1",standAlone=True)
 #This will be replaced with corresponding DUT Ip and port while executing script
 ip = <ipaddress>
 port = <port>
-obj.configureTestCase(ip,port,'RDKV_CERT_MVS_Video_Play_Widevine_Cypt_Skip_Byte_Block_DASH_AC3')
+obj.configureTestCase(ip,port,'RDKV_CERT_MVS_Video_Play_Widevine_Crypt_Skip_Byte_Block_DASH_HEVC_AAC')
 
 webkit_console_socket = None
 
@@ -62,7 +62,7 @@ if expectedResult in result.upper():
         conf_file,result = getDeviceConfigFile(obj.realpath)
         setDeviceConfigFile(conf_file)
         #appURL    = MediaValidationVariables.lightning_video_test_app_url
-        videoURL  = MediaValidationVariables.video_src_url_widevine_crypt_skip_block_dash_h264_ac3
+        videoURL  = MediaValidationVariables.video_src_url_widevine_crypt_skip_block_dash_hevc_aac
         # Setting VideoPlayer Operations
         setOperation("close",MediaValidationVariables.close_interval)
         operations = getOperations()
@@ -70,20 +70,20 @@ if expectedResult in result.upper():
         setURLArgument("url",videoURL)
         setURLArgument("operations",operations)
         setURLArgument("autotest","true")
-        setURLArgument("drmconfigs",MediaValidationVariables.video_src_url_widevine_crypt_skip_block_dash_h264_ac3_drmconfigs)
+        setURLArgument("drmconfigs",MediaValidationVariables.video_src_url_widevine_crypt_skip_block_dash_hevc_aac_drmconfigs)
         setURLArgument("type","dash")
         appArguments = getURLArguments()
 
         # Getting the complete test app URL for selected players
         video_test_urls = []
         test_counter = 0
-        players_list = str(MediaValidationVariables.codec_ac3).split(",")
+        players_list = str(MediaValidationVariables.codec_dash_hevc).split(",")
         print("SELECTED PLAYERS: ", players_list)
         video_test_urls = getTestURLs(players_list,appArguments)
 
         #Example video test url
         #http://*testManagerIP*/rdk-test-tool/fileStore/lightning-apps/unifiedplayer/build/index.html?
-        #url=<video_ac3_url>.mpd&drmconfigs=com.widevine(<license_url>)&operations=close(60)&autotest=true&type=dash
+        #url=<video_hevc_aac_url>.mpd&drmconfigs=com.widevine(<license_url>)&operations=close(60)&autotest=true&type=dash
 
         # Setting the video test url in PersistentStore and launching the test app using AppManager
         for video_test_url in video_test_urls:
